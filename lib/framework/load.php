0ce6256f2072e6f8a41a6d358f445d086cfd1abd
Glenn Ansley <glenn@ithemes.com>
2013-01-16 07:19:38 -0600

    Delete unneeded folders

--- lib/framework/load.php
+++ lib/framework/load.php
@@ -6,9 +6,3 @@
 
 // Include Core Post Types that will always be created
 require_once( $this->_plugin_path . '/lib/framework/class.post-types.php' );
-
-/*
-require_once( $this->_plugin_path . '/lib/framework/post-types/product.php' );
-require_once( $this->_plugin_path . '/lib/framework/post-types/transaction.php' );
-require_once( $this->_plugin_path . '/lib/framework/post-types/cart.php' );
-*/
