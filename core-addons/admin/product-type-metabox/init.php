fa30ab461a2b48a513c7a03996c49ee4fefac274
Glenn Ansley <glenn@ithemes.com>
2013-04-12 08:39:57 -0500

    Exchange rename: core-addons/product-type-metabox

--- core-addons/admin/product-type-metabox/init.php
+++ core-addons/admin/product-type-metabox/init.php
@@ -3,9 +3,9 @@
  * This is a core add-on. It adds the Product Type metabox to the New / Edit Product view
  *
  * @since 0.3.1
- * @package IT_Cart_Buddy
+ * @package IT_Exchange
 */
-class IT_Cart_Buddy_Core_Addon_Product_Type_Meta_Box {
+class IT_Exchange_Core_Addon_Product_Type_Meta_Box {
 	
 	/**
 	 * Class constructor. Registers hooks
@@ -13,10 +13,10 @@ class IT_Cart_Buddy_Core_Addon_Product_Type_Meta_Box {
 	 * @since 0.3.1
 	 * @return void
 	*/
-	function IT_Cart_Buddy_Core_Addon_Product_Type_Meta_Box() {
+	function IT_Exchange_Core_Addon_Product_Type_Meta_Box() {
 		if ( is_admin() ) {
-			add_action( 'it_cart_buddy_product_metabox_callback', array( $this, 'register_product_type_meta_box' ) );
-			add_action( 'it_cart_buddy_save_product', array( $this, 'update_post_product_type' ) );
+			add_action( 'it_exchange_product_metabox_callback', array( $this, 'register_product_type_meta_box' ) );
+			add_action( 'it_exchange_save_product', array( $this, 'update_post_product_type' ) );
 		}
 	}
 
@@ -28,9 +28,9 @@ class IT_Cart_Buddy_Core_Addon_Product_Type_Meta_Box {
 	*/
 	function register_product_type_meta_box( $post ) {
 		global $pagenow;
-		$product_types = it_cart_buddy_get_enabled_addons( array( 'category' => 'product-type' ) );
+		$product_types = it_exchange_get_enabled_addons( array( 'category' => 'product-type' ) );
 		if ( is_array( $product_types ) && count( $product_types ) > 1  && $pagenow != 'post-new.php' )
-			add_meta_box( 'it_cart_buddy_product_type', __( 'Product Type', 'LION' ), array( $this, 'print_meta_box' ), $post->post_type, 'side' );
+			add_meta_box( 'it_exchange_product_type', __( 'Product Type', 'LION' ), array( $this, 'print_meta_box' ), $post->post_type, 'side' );
 	}
 
 	/**
@@ -40,9 +40,9 @@ class IT_Cart_Buddy_Core_Addon_Product_Type_Meta_Box {
 	 * @void
 	*/
 	function print_meta_box( $post ) {
-		$product = it_cart_buddy_get_product( $post );
+		$product = it_exchange_get_product( $post );
 
-		$enabled              = it_cart_buddy_get_enabled_addons( array( 'category' => array( 'product-type' ) ) );
+		$enabled              = it_exchange_get_enabled_addons( array( 'category' => array( 'product-type' ) ) );
 		$current_product_type = $product->product_type;
 
 		echo '<p class="description">' . __( 'You must save the product after changing this to access new product options.', 'LION' ) . '</p>';
@@ -51,14 +51,14 @@ class IT_Cart_Buddy_Core_Addon_Product_Type_Meta_Box {
 		} else if ( count( $enabled ) === 1 ) {
 			$product_type = reset( $enabled );
 			echo '<p>' . esc_attr( $product_type['name'] ) . '</p>';
-			echo '<input type="hidden" name="_it_cart_buddy_product_type" value="' . esc_attr( $product_type['slug'] ) . '" />';
+			echo '<input type="hidden" name="_it_exchange_product_type" value="' . esc_attr( $product_type['slug'] ) . '" />';
 			echo '<p class="description">' . __( 'You must have more than 1 product type enabled to make changes here.', 'LION' ) . '</p>';
 		} else {
-			?><div id="it_cart_buddy_product_type_select"><?php
+			?><div id="it_exchange_product_type_select"><?php
 			foreach( $enabled as $slug => $params ) {
 				?>
-				<label for="it_cart_buddy_product_type-<?php esc_attr_e( $slug ); ?>">
-					<input type="radio" id="it_cart_buddy_product_type-<?php esc_attr_e( $slug ); ?>" name="_it_cart_buddy_product_type" <?php checked( $slug, $current_product_type ); ?> value="<?php esc_attr_e( $slug ); ?>" /> <?php esc_attr_e( $params['name'] ); ?><br />
+				<label for="it_exchange_product_type-<?php esc_attr_e( $slug ); ?>">
+					<input type="radio" id="it_exchange_product_type-<?php esc_attr_e( $slug ); ?>" name="_it_exchange_product_type" <?php checked( $slug, $current_product_type ); ?> value="<?php esc_attr_e( $slug ); ?>" /> <?php esc_attr_e( $params['name'] ); ?><br />
 				</label>
 				<?php
 			}
@@ -81,18 +81,18 @@ class IT_Cart_Buddy_Core_Addon_Product_Type_Meta_Box {
 		if ( empty( $post->ID ) )
 			return;
 
-		if ( ! $product = it_cart_buddy_get_product( $post ) )
+		if ( ! $product = it_exchange_get_product( $post ) )
 			return;
 
 		if ( ! $product->ID )
 			return;
 
 		// If there is an updated product type in the POST array, use that. Otherwise, use the url param if not empty.
-		$product_type = empty ( $_POST['_it_cart_buddy_product_type'] ) && ! empty( $_GET['product_type'] ) ? $_GET['product_type'] : $_POST['_it_cart_buddy_product_type'];
+		$product_type = empty ( $_POST['_it_exchange_product_type'] ) && ! empty( $_GET['product_type'] ) ? $_GET['product_type'] : $_POST['_it_exchange_product_type'];
 
 		// If we have a product_type, update
 		if ( $product_type )
-			update_post_meta( $post->ID, '_it_cart_buddy_product_type', $product_type );
+			update_post_meta( $post->ID, '_it_exchange_product_type', $product_type );
 	}
 }
-new IT_Cart_Buddy_Core_Addon_Product_Type_Meta_Box(); 
+new IT_Exchange_Core_Addon_Product_Type_Meta_Box(); 
