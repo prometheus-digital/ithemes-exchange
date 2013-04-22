19919512f61eab8be915abf4f19d89fb8d5446fe
Glenn Ansley <glenn@ithemes.com>
2013-04-22 15:47:57 -0500

    Add JS file for add-edit-products

--- lib/admin/class.admin.php
+++ lib/admin/class.admin.php
@@ -695,6 +695,9 @@ class IT_Exchange_Admin {
 		// Enqueue styles
 		wp_enqueue_style( 'it-exchange-add-edit-product', ITUtility::get_url_from_file( dirname( __FILE__ ) ) . '/styles/add-edit-product.css' );
 
+		// Enqueue scripts
+		wp_enqueue_script( 'it-exchange-add-edit-product', ITUtility::get_url_from_file( dirname( __FILE__ ) ) . '/js/add-edit-product.js', array( 'jquery' ) );
+
 		// Temporarially remove post support for post_formats and title
 		add_filter( 'post_updated_messages', array( $this, 'temp_remove_theme_supports' ) ); 
 
