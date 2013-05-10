f96b2cc531c1e9e1fcd9d9960ea704df721c8e79
Glenn Ansley <glenn@ithemes.com>
2013-05-10 08:25:36 -0500

    Create load file for product-features

--- lib/load.php
+++ lib/load.php
@@ -18,38 +18,8 @@ require( $this->_plugin_path . 'lib/products/class.products-post-type.php' );
 // Product Object
 require( $this->_plugin_path . 'lib/products/class.product.php' );
 
-// Product Feature: Title
-require( $this->_plugin_path . 'lib/product-features/class.title.php' );
-
-// Product Feature: Base Price
-require( $this->_plugin_path . 'lib/product-features/class.base-price.php' );
-
-// Product Feature: Product Description
-require( $this->_plugin_path . 'lib/product-features/class.description.php' );
-
-// Product Feature: Downloads
-require( $this->_plugin_path . 'lib/product-features/class.downloads.php' );
-
-// Product Feature: Visibility
-require( $this->_plugin_path . 'lib/product-features/class.product-status.php' );
-
-// Product Feature: Product Images 
-require( $this->_plugin_path . 'lib/product-features/class.product-images.php' );
-
-// Product Feature: Purchase Message 
-require( $this->_plugin_path . 'lib/product-features/class.purchase-message.php' );
-
-// Product Feature: Product Availability 
-require( $this->_plugin_path . 'lib/product-features/class.product-availability.php' );
-
-// Product Feature: Quantity
-require( $this->_plugin_path . 'lib/product-features/class.purchase-quantity.php' );
-
-// Product Feature: Inventory
-require( $this->_plugin_path . 'lib/product-features/class.inventory.php' );
-
-// Product Features: WP Post Type Supports as Product Features
-require( $this->_plugin_path . 'lib/product-features/class.wp-post-supports.php' );
+// Product Features
+require( $this->_plugin_path . 'lib/product-features/load.php' );
 
 // Transaction Post Type
 require( $this->_plugin_path . 'lib/transactions/class.transactions-post-type.php' );
