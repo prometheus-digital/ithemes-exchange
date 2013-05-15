06b80c97510983ef72c3bf470b0e4d299484c4a6
Justin Kopepasah <justin@kopepasah.com>
2013-05-15 11:52:39 -0500

    Committing the nearly completed wizard.

--- lib/admin/views/admin-wizard.php
+++ lib/admin/views/admin-wizard.php
@@ -51,36 +51,31 @@
 				
 				<?php do_action( 'it_exchange_print_wizard_settings', $form ); ?>
 				
-				<div class="field company-email">
+				<div class="field general-settings-wizard">
 					<h3><?php _e( 'General', 'LION' ); ?></h3>
 					<label for="company-email"><?php _e( 'E-mail Notifications', 'LION' ); ?> <span class="tip" title="<?php _e( 'The E-mail address you should receive notifcations to, from your store.', 'LION' ); ?>">i</span></label>
-					<?php $form->add_text_box( 'company-email', get_bloginfo( 'admin_email' ) ); ?>
+					<?php $form->add_text_box( 'company-email', array( 'value' => get_bloginfo( 'admin_email' ), 'class' => 'clearfix' ) ); ?>
 					<?php $form->add_check_box( 'exchange-notifications' ); ?>
 					<label for="exchange-notifications"><?php _e( 'Get e-mail updates from us about iThemes Exchange', 'LION' ); ?> <span class="tip" title="<?php _e( "We'll send you updates, discounts on add-ons and other iThemes products, and our eternal love.", 'LION' ); ?>">i</span></label>
-
-				</div>
-			
-				<div class="field default-currency">
-					<label for="default-currency"><?php _e( 'Currency', 'LION' ); ?> <span class="tip" title="<?php _e( 'Select the currenc you plan on using in your store.', 'LION' ); ?>">i</span></label>
-					<?php $form->add_drop_down( 'default-currency', $this->get_default_currency_options() ); ?>
+					<div class="default-currency">
+						<label for="default-currency"><?php _e( 'Currency', 'LION' ); ?> <span class="tip" title="<?php _e( 'Select the currenc you plan on using in your store.', 'LION' ); ?>">i</span></label>
+						<?php $form->add_drop_down( 'default-currency', $this->get_default_currency_options() ); ?>
+					</div>
 				</div>
 				
-				<div class="clearfix"></div>
-				<br>
+				<div class="field add-on-banner">
+					<img src="<?php echo ITUtility::get_url_from_file( dirname( dirname( __FILE__ ) ) . '/images/icon32.png' ); ?>" />
+					<p><?php _e( 'You\'re almost ready to start selling digital products using PayPal and iThemes Exchange.', 'LION' ); ?></p>
+					<p><strong><?php _e( 'Remember, if you want to do more with Exchange, check out our Add-Ons Library.', 'LION' ); ?></strong></p>
+					<a class="get-add-ons " href="javascript:void(0);" target="_blank"><span><?php _e( "Get Add-Ons", 'LION' ); ?></span></a>
+				</div>
 				
-				<div class="clearfix"></div>
-				<br>
-				
-				<?php $form->add_submit( 'submit', __( 'Start Selling!', 'LION' ) ); ?>
-				<?php $form->add_hidden( 'dismiss-wizard-nag', true ); ?>
-				<?php $form->add_hidden( 'wizard-submitted', true ); ?>
+				<div class="field submit-wrapper">
+					<?php $form->add_submit( 'submit', __( 'Start Selling!', 'LION' ) ); ?>
+					<?php $form->add_hidden( 'dismiss-wizard-nag', true ); ?>
+					<?php $form->add_hidden( 'wizard-submitted', true ); ?>
+				</div>
 			</div>
-	        <div class="add-on-banner">
-	            <a class="addon-banner" href="#" target="_blank">
-	                <p><?php _e( "You're almost ready to start selling digital products using PayPal and iThemes Exchange. <strong>Remember, if you want to do more with Exchange, check out our Add-Ons Library</strong>.", 'LION' ); ?></p>
-	                <span><?php _e( "Get Add-Ons", 'LION' ); ?></span>
-	            </a>
-	        </div>
 		</div>
 	<?php $form->end_form(); ?> 
 </div>
