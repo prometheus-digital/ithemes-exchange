5d259eb09c674645be1ae4d164138f76d2b0fed7
Justin Kopepasah <justin@kopepasah.com>
2013-05-23 18:35:35 -0500

    Added the profile awesomeness.

--- lib/templates/content-profile.php
+++ lib/templates/content-profile.php
@@ -4,47 +4,46 @@
  *
  * @since 0.4.0
  */
+
+// it_exchange( 'customer', 'username' );
+// it_exchange( 'customer', 'accountmenu' );
+// it_exchange( 'customer', 'avatar' );
+// it_exchange( 'customer', 'username' );
 ?>
 
 <div class="customer-info">
-    
-    <?php it_exchange( 'customer', 'formopen' ); ?>
-    
-	<?php it_exchange_get_template_part( 'messages' ); ?>
-    
-    <div class="user-name">
-    <?php it_exchange( 'customer', 'username' ); ?>
-    </div>
-    <div class="account-menu-nav">
-    <?php it_exchange( 'customer', 'accountmenu' ); ?>
-    </div>
-    <div class="customer-avatar">
-    <?php it_exchange( 'customer', 'avatar' ); ?>
-    </div>
-    <div class="first-name">
-    <?php it_exchange( 'customer', 'firstname' ); ?>
-    </div>
-    <div class="last-name">
-    <?php it_exchange( 'customer', 'lastname' ); ?>
-    </div>
-    <div class="user-name">
-    <?php it_exchange( 'customer', 'username' ); ?>
-    </div>
-    <div class="email-name">
-    <?php it_exchange( 'customer', 'email' ); ?>
-    </div>
-    <div class="website">
-    <?php it_exchange( 'customer', 'website' ); ?>
-    </div>
-    <div class="password1">
-    <?php it_exchange( 'customer', 'password1' ); ?>
-    </div>
-    <div class="password2">
-    <?php it_exchange( 'customer', 'password2' ); ?>
-    </div>
-
-    <?php it_exchange( 'customer', 'save' ); ?>
-    
-    <?php it_exchange( 'customer', 'formclose' ); ?>
-
-</div><!-- .customer-info -->
+	<?php it_exchange( 'customer', 'formopen' ); ?>
+		<?php it_exchange_get_template_part( 'messages' ); ?>
+		
+		<div class="customer-name">
+			<div class="first-name">
+				<?php it_exchange( 'customer', 'firstname' ); ?>
+			</div>
+			<div class="last-name">
+				<?php it_exchange( 'customer', 'lastname' ); ?>
+			</div>
+		</div>
+		
+		<div class="customer-email">
+			<?php it_exchange( 'customer', 'email' ); ?>
+		</div>
+		
+		<div class="customer-website">
+			<?php it_exchange( 'customer', 'website' ); ?>
+		</div>
+		
+		<div class="customer-password">
+			<div class="password-1">
+				<?php it_exchange( 'customer', 'password1' ); ?>
+			</div>
+			<div class="password-2">
+				<?php it_exchange( 'customer', 'password2' ); ?>
+			</div>
+		</div>
+		
+		<div class="customer-save">
+			<?php it_exchange( 'customer', 'save' ); ?>
+		</div>
+		
+	<?php it_exchange( 'customer', 'formclose' ); ?>
+</div>
