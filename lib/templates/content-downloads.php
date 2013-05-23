8ac2a1aec55683245e38e550bb04444405f3b6b9
Justin Kopepasah <justin@kopepasah.com>
2013-05-23 18:14:46 -0500

    Sprucing up the downloads.

--- lib/templates/content-downloads.php
+++ lib/templates/content-downloads.php
@@ -1,1 +1,45 @@
-<p>Downloads go here!</p>
+<?php
+/**
+ * The default template for displaying a customer downloads.
+ */
+?>
+
+<div class="downloads-wrapper">
+	<div class="download">
+		<div class="download-get">
+			<a href class="button">Download</a>
+		</div>
+		<div class="download-info">
+			<h4>Download Title</h4>
+			<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+		</div>
+	</div>
+	<div class="download">
+		<div class="download-get">
+			<a href class="button">Download</a>
+		</div>
+		<div class="download-info">
+			<h4>Download Title</h4>
+			<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+		</div>
+	</div>
+	<div class="download">
+		<div class="download-get">
+			<a href class="button">Download</a>
+		</div>
+		<div class="download-info">
+			<h4>Download Title</h4>
+			<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+		</div>
+	</div>
+	<div class="download">
+		<div class="download-get">
+			<a href class="button">Download</a>
+		</div>
+		<div class="download-info">
+			<h4>Download Title</h4>
+			<p>Etiam porta sem malesuada magna mollis euismod. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+			<span>Downloads Remaining: <strong>3</strong></span> <span>Product Expires <strong>5/11/14</strong></span>
+		</div>
+	</div>
+</div>
