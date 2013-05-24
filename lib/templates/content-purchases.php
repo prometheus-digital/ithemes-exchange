d23086d06acef02ca1508645f51188afc1dbc198
Justin Kopepasah <justin@kopepasah.com>
2013-05-24 16:23:25 -0500

    Styling up the purchases.

--- lib/templates/content-purchases.php
+++ lib/templates/content-purchases.php
@@ -1,1 +1,49 @@
-<p>Purchases go here!</p>
+<?php
+/**
+ * The default template for displaying a customer it-exchange-purchases.
+ */
+?>
+
+<div class="it-exchange-purchases-wrapper">
+	<div class="it-exchange-purchase">
+		<div class="it-exchange-purchase-top">
+			<span class="it-exchange-purchase-date"><strong>5/12/13</strong></span> <span class="it-exchange-purchase-status">- Pending</span> <span class="it-exchange-purchase-total"><strong>$14.00</strong></span>
+		</div>
+		<div class="it-exchange-purchase-items">
+			<div class="item-info">
+				<div class="item-thumbnail">
+					<img src="http://placehold.it/150x150"/>
+				</div>
+				<div class="item-data">
+					<h4>A Picture of a Tree <span class="item-price">- $11.00</span></h4>
+					<p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus.</p>
+				</div>
+			</div>
+			<div class="item-info">
+				<div class="item-thumbnail">
+					<img src="http://placehold.it/150x150"/>
+				</div>
+				<div class="item-data">
+					<h4>So You Want to be a Freelancer (Ebook) <span class="item-price">- $3.00</span></h4>
+					<p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="it-exchange-purchase">
+		<div class="it-exchange-purchase-top">
+			<span class="it-exchange-purchase-date"><strong>4/6/13</strong></span> <span class="it-exchange-purchase-status">- Delivered</span> <span class="it-exchange-purchase-total"><strong>$0.99</strong></span>
+		</div>
+		<div class="it-exchange-purchase-items">
+			<div class="item-info">
+				<div class="item-thumbnail">
+					<img src="http://placehold.it/150x150"/>
+				</div>
+				<div class="item-data">
+					<h4>The Entrepreneurial Adventure: Is It For You? (Ebook) <span class="item-price">- $0.99</span></h4>
+					<p>Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
