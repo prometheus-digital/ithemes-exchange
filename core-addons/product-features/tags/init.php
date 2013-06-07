4208d78e140dceba34447ddcf03db3198a1ef852
Matt Danner <matt@ithemes.com>
2013-06-07 14:05:29 -0500

    Whitespace and tab cleanup for /core-addons/product-features/tags/

--- core-addons/product-features/tags/init.php
+++ core-addons/product-features/tags/init.php
@@ -5,9 +5,9 @@
  * @package iThemes Exchange
  * @since 0.4.0
  */
- 
+
 if ( !function_exists( 'create_it_exchange_tags' ) ) {
-		
+
 	/**
 	 * Registers iThemes Exchange Product Tag Taxonomy
 	 *
@@ -15,39 +15,39 @@ if ( !function_exists( 'create_it_exchange_tags' ) ) {
 	 * @uses register_taxonomy()
 	 */
 	function create_it_exchange_tags() {
-		
-	  $labels = array(
-			'name' 				=> __( 'Product Tags', 'LION' ),
-			'singular_name' 	=> __( 'Product Tag', 'LION' ),
-			'search_items'		=> __( 'Search Product Tags', 'LION' ),
-			'all_items' 		=> __( 'All Product Tags', 'LION' ), 
-			'parent_item' 		=> __( 'Parent Product Tags', 'LION' ),
+
+		$labels = array(
+			'name'              => __( 'Product Tags', 'LION' ),
+			'singular_name'     => __( 'Product Tag', 'LION' ),
+			'search_items'      => __( 'Search Product Tags', 'LION' ),
+			'all_items'         => __( 'All Product Tags', 'LION' ),
+			'parent_item'       => __( 'Parent Product Tags', 'LION' ),
 			'parent_item_colon' => __( 'Parent Product Tags:', 'LION' ),
-			'edit_item' 		=> __( 'Edit Product Tags', 'LION' ), 
-			'update_item' 		=> __( 'Update Product Tags', 'LION' ),
-			'add_new_item' 		=> __( 'Add New Product Tags', 'LION' ),
-			'new_item_name' 	=> __( 'New Product Tag', 'LION' ),			
-		); 	
-	
+			'edit_item'         => __( 'Edit Product Tags', 'LION' ),
+			'update_item'       => __( 'Update Product Tags', 'LION' ),
+			'add_new_item'      => __( 'Add New Product Tags', 'LION' ),
+			'new_item_name'     => __( 'New Product Tag', 'LION' ),
+		);
+
 		register_taxonomy(
-			'it_exchange_tag', 
+			'it_exchange_tag',
 			array( 'it_exchange_prod' ),
 			array(
-				'hierarchical' 	=> false,
-				'labels' 		=> $labels,
-				'show_ui' 		=> true,
-				'query_var' 	=> true,
-				'rewrite' 		=> array( 'slug' => 'product-tag' ),
+				'hierarchical' => false,
+				'labels'       => $labels,
+				'show_ui'      => true,
+				'query_var'    => true,
+				'rewrite'      => array( 'slug' => 'product-tag' ),
 			)
 		);
-		
+
 	}
 	add_action( 'init', 'create_it_exchange_tags', 0 );
 
 }
 
 if ( !function_exists( 'it_exchange_tags_add_menu_item' ) ) {
-			
+
 	/**
 	 * This adds a menu item to the Exchange menu pointing to the WP All [post_type] table
 	 *
