6561c3695a7780c2ea34d9d1891e6cb5882e16ef
Justin Kopepasah <justin@kopepasah.com>
2013-06-10 11:30:57 -0500

    Finalized the multi item super widget purchase process.

--- lib/templates/super-widget-product.php
+++ lib/templates/super-widget-product.php
@@ -1,13 +1,15 @@
 <?php if ( it_exchange( 'product', 'found' ) ) : ?>
-	<?php if ( it_exchange( 'cart', 'get-item-count' ) && it_exchange_is_multi_item_cart_allowed() ) : ?>
-		<div class="item-count">
-			<?php printf( __( 'You have %s item(s) in your <a href="%s">cart</a>', 'LION' ), it_exchange( 'cart', 'get-item-count' ), it_exchange_get_page_url( 'cart' ) ); ?>
-		</div>
-	<?php endif; ?>
-
-	<?php if ( it_exchange_is_view( 'product' ) ) : ?>
-	<div class="it-exchange-product">
-		<?php it_exchange( 'product', 'purchase-options' ); ?>
+	<div class="it-exchange-sw-selecting">
+		<?php if ( it_exchange( 'cart', 'get-item-count' ) && it_exchange_is_multi_item_cart_allowed() ) : ?>
+			<div class="item-count">
+				<?php printf( __( 'You have %s item(s) in your <a href="%s">cart</a>', 'LION' ), it_exchange( 'cart', 'get-item-count' ), it_exchange_get_page_url( 'cart' ) ); ?>
+			</div>
+		<?php endif; ?>
+	
+		<?php if ( it_exchange_is_view( 'product' ) ) : ?>
+			<div class="purchase-options">
+				<?php it_exchange( 'product', 'purchase-options', array( 'edit-quantity' => false ) ); ?>
+			</div>
+		<?php endif; ?>
 	</div>
-	<?php endif; ?>
 <?php endif; ?>
