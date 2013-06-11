3300be032aed848018a573d416437217359eb938
Glenn Ansley <glenn@ithemes.com>
2013-06-11 08:17:51 -0500

    Remove Transaction Details JS and CSS from Payments list in Admin

--- lib/admin/class.admin.php
+++ lib/admin/class.admin.php
@@ -1130,7 +1130,7 @@ Thank you for your order. Your order's details are below.
 		if ( isset( $post_type ) && 'it_exchange_prod' === $post_type ) {
 			$deps = array( 'jquery-ui-sortable', 'jquery-ui-droppable', 'jquery-ui-tabs' );
 			wp_enqueue_script( 'it-exchange-add-edit-product', ITUtility::get_url_from_file( dirname( __FILE__ ) ) . '/js/add-edit-product.js', $deps );
-		} else if ( isset( $post_type ) && 'it_exchange_tran' === $post_type ) {
+		} else if ( isset( $post_type ) && 'it_exchange_tran' === $post_type && ! empty( $_GET['action'] ) && 'edit' == $_GET['action'] ) {
 			$deps = array();
 			wp_enqueue_script( 'it-exchange-transaction-details', ITUtility::get_url_from_file( dirname( __FILE__ ) ) . '/js/transaction-details.js', $deps );
 		} else if ( 'exchange_page_it-exchange-addons' === $hook_suffix ) {
@@ -1177,7 +1177,7 @@ Thank you for your order. Your order's details are below.
 
 		if ( isset( $post_type ) && 'it_exchange_prod' === $post_type ) {
 			wp_enqueue_style( 'it-exchange-add-edit-product', ITUtility::get_url_from_file( dirname( __FILE__ ) ) . '/styles/add-edit-product.css' );
-		} else if ( isset( $post_type ) && 'it_exchange_tran' === $post_type ) {
+		} else if ( isset( $post_type ) && 'it_exchange_tran' === $post_type && ! empty( $_GET['action'] ) && 'edit' == $_GET['action'] ) {
 			wp_enqueue_style( 'it-exchange-transaction-details', ITUtility::get_url_from_file( dirname( __FILE__ ) ) . '/styles/transaction-details.css' );
 		} else if ( 'exchange_page_it-exchange-addons' === $hook_suffix ) {
 			wp_enqueue_style( 'it-exchange-add-ons', ITUtility::get_url_from_file( dirname( __FILE__ ) ) . '/styles/add-ons.css' );
