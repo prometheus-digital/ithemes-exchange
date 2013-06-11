e1411620b0dd1fe16dea129a61540e9684bfaf74
Lew Ayotte <lew@lewayotte.com>
2013-06-11 13:24:51 -0400

    added update customer info button for info tab for customer data section of user profile, disabled manual transaction button for pre-launch

--- lib/admin/views/admin-user-profile.php
+++ lib/admin/views/admin-user-profile.php
@@ -7,16 +7,22 @@
 // Just adding internal CSS rule here since it won't be around long.
 ?>
 
-<div class="it-exchange-customer-data-box">
-	<?php
-	if ( empty( $_REQUEST['user_id'] ) )
-		$user_id = get_current_user_id();
-	else
-		$user_id = $_REQUEST['user_id'];
-		
-	$user_object = get_userdata( $user_id );
-	?>
+<?php
+if ( empty( $_REQUEST['user_id'] ) )
+    $user_id = get_current_user_id();
+else
+    $user_id = $_REQUEST['user_id'];
     
-	<?php _e( 'This user is an Exchange customer.', 'LION' );
-	echo "<a class='it-exchange-cust-info' href='" . esc_url( add_query_arg( array( 'wp_http_referer' => urlencode( stripslashes_deep( $_SERVER['REQUEST_URI'] ) ), 'it_exchange_customer_data' => 1 ), get_edit_user_link( $user_object->ID ) ) ) . "'>" . __( 'View Customer Data', 'LION' ) . "</a>"; ?>
-</div>
+$user_object = get_userdata( $user_id );
+?>
+
+<table class="form-table">
+<tbody>
+<tr id="it_exchange_customer_data">
+    <th><?php _e( 'This user is an Exchange customer.', 'LION' ); ?></th>
+    <td>
+    <?php echo "<a class='it-exchange-cust-info' href='" . esc_url( add_query_arg( array( 'wp_http_referer' => urlencode( stripslashes_deep( $_SERVER['REQUEST_URI'] ) ), 'it_exchange_customer_data' => 1 ), get_edit_user_link( $user_object->ID ) ) ) . "'>" . __( 'View Customer Data', 'LION' ) . "</a>"; ?>
+    </td>
+</tr>
+</tbody>
+</table>
