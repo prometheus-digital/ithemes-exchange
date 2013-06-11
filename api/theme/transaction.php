f716862fd4c2a5a47f58633c7396c5b44c2e4cf3
Glenn Ansley <glenn@ithemes.com>
2013-06-11 11:45:28 -0500

    Transaction Theme API.\n Frontend Confirmation Page Wireframe

--- api/theme/transaction.php
+++ api/theme/transaction.php
@@ -27,12 +27,24 @@ class IT_Theme_API_Transaction implements IT_Theme_API {
 	 * @since 0.4.0
 	*/
 	public $_tag_map = array(
-		'status'       => 'status',
-		'date'         => 'date',
-		'total'        => 'total',
-		'products'     => 'products',
-		'instructions' => 'instructions',
+		'status'                 => 'status',
+		'date'                   => 'date',
+		'total'                  => 'total',
+		'instructions'           => 'instructions',
+		'products'               => 'products',
+		'productattribute'       => 'product_attribute',
+		'productdownloads'       => 'product_downloads',
+		'productdownload'        => 'product_download',
+		'productdownloadhashes'  => 'product_download_hashes',
+		'productdownloadhash'    => 'product_download_hash',
 	);
+
+	/**
+	 * The current transaction product
+	 * @var array $_transaction_product
+	 * @since 0.4.0
+	*/
+	public $_transaction_product = false;
 
 	/**
 	 * Constructor
@@ -42,7 +54,10 @@ class IT_Theme_API_Transaction implements IT_Theme_API {
 	 * @return void
 	*/
 	function IT_Theme_API_Transaction() {
-		$this->_transaction = empty( $GLOBALS['it_exchange']['transaction'] ) ? false : $GLOBALS['it_exchange']['transaction'];
+		$this->_transaction         = empty( $GLOBALS['it_exchange']['transaction'] ) ? false : $GLOBALS['it_exchange']['transaction'];
+		$this->_transaction_product = empty( $GLOBALS['it_exchange']['transaction_product'] ) ? false : $GLOBALS['it_exchange']['transaction_product'];
+		$this->_transaction_product_download = empty( $GLOBALS['it_exchange']['transaction_product_download'] ) ? false : $GLOBALS['it_exchange']['transaction_product_download'];
+		$this->_transaction_product_download_hash = empty( $GLOBALS['it_exchange']['transaction_product_download_hash'] ) ? false : $GLOBALS['it_exchange']['transaction_product_download_hash'];
 	}
 
 	/**
@@ -131,7 +146,7 @@ class IT_Theme_API_Transaction implements IT_Theme_API {
 	}
 
     /** 
-     * This loops through the products GLOBAL and updates the product global.
+     * This loops through the transaction_products GLOBAL and updates the transaction_product global.
      *
      * It return false when it reaches the last product
      * If the has flag has been passed, it just returns a boolean
@@ -144,23 +159,171 @@ class IT_Theme_API_Transaction implements IT_Theme_API {
         if ( $options['has'] )
             return count( it_exchange_get_transaction_products( $this->_transaction ) ) > 0 ; 
 
-        // If we made it here, we're doing a loop of products for the current query.
-        // This will init/reset the products global and loop through them. the /api/theme/product.php file will handle individual products.
-        if ( empty( $GLOBALS['it_exchange']['products'] ) ) { 
-            $GLOBALS['it_exchange']['products'] = it_exchange_get_transaction_products( $this->_transaction );
-            $GLOBALS['it_exchange']['product'] = reset( $GLOBALS['it_exchange']['products'] );
+        // If we made it here, we're doing a loop of transaction_products for the current query.
+        // This will init/reset the transaction_products global and loop through them.
+        if ( empty( $GLOBALS['it_exchange']['transaction_products'] ) ) { 
+            $GLOBALS['it_exchange']['transaction_products'] = it_exchange_get_transaction_products( $this->_transaction );
+            $GLOBALS['it_exchange']['transaction_product'] = reset( $GLOBALS['it_exchange']['transaction_products'] );
             return true;
         } else {
-            if ( next( $GLOBALS['it_exchange']['products'] ) ) { 
-                $GLOBALS['it_exchange']['product'] = current( $GLOBALS['it_exchange']['products'] );
+            if ( next( $GLOBALS['it_exchange']['transaction_products'] ) ) { 
+                $GLOBALS['it_exchange']['transaction_product'] = current( $GLOBALS['it_exchange']['transaction_products'] );
                 return true;
             } else {
-                $GLOBALS['it_exchange']['product'] = false;
+                $GLOBALS['it_exchange']['transaction_product'] = false;
                 return false;
             }   
         }   
-        end( $GLOBALS['it_exchange']['products'] );
-        $GLOBALS['it_exchange']['product'] = false;
+        end( $GLOBALS['it_exchange']['transaction_products'] );
+        $GLOBALS['it_exchange']['transaction_product'] = false;
         return false;
     }
+
+	/** 
+	 * The product title
+	 *
+	 * @since 0.4.0
+	 * @return string
+	*/
+	function product_attribute( $options=array() ) { 
+
+		// Set defaults
+		$defaults = array(
+			'wrap'      => false,
+			'format'    => 'html',
+			'attribute' => false,
+		);  
+		$options = ITUtility::merge_defaults( $options, $defaults );
+
+		// Return empty if attribute was not provided
+		if ( empty( $options['attribute'] ) )
+			return '';
+
+		// Return empty string if empty
+		if ( ! $attribute = it_exchange_get_transaction_product_feature( $this->_transaction_product, $options['attribute'] ) )
+			return '';
+
+		$open_wrap  = empty( $options['wrap'] ) ? '' : '<' . esc_attr( $options['wrap'] ) . ' class="entry-title">';
+		$close_wrap = empty( $options['wrap'] ) ? '' : '</' . esc_attr( $options['wrap'] ) . '>';
+		$result   = ''; 
+
+		if ( 'html' == $options['format'] )
+			$result .= $open_wrap;
+
+		$result .= $attribute;
+
+		if ( 'html' == $options['format'] )
+			$result .= $close_wrap;
+
+		return $result;
+	} 
+
+	function product_downloads( $options=array() ) {
+		// Return false if we don't have a product id
+		if ( empty( $this->_transaction_product['product_id'] ) )
+			return false;
+
+		// Return boolean if we'er just checking
+		if ( ! empty( $options['has'] ) )
+			return it_exchange_product_has_feature( $this->_transaction_product['product_id'], 'downloads' );
+
+        // If we made it here, we're doing a loop of transaction_product_downloads for the current query.
+        // This will init/reset the transaction_product_downloads global and loop through them.
+        if ( empty( $GLOBALS['it_exchange']['transaction_product_downloads'] ) ) { 
+            $GLOBALS['it_exchange']['transaction_product_downloads'] = it_exchange_get_product_feature( $this->_transaction_product['product_id'], 'downloads' );
+            $GLOBALS['it_exchange']['transaction_product_download'] = reset( $GLOBALS['it_exchange']['transaction_product_downloads'] );
+            return true;
+        } else {
+            if ( next( $GLOBALS['it_exchange']['transaction_product_downloads'] ) ) { 
+                $GLOBALS['it_exchange']['transaction_product_download'] = current( $GLOBALS['it_exchange']['transaction_product_downloads'] );
+                return true;
+            } else {
+                $GLOBALS['it_exchange']['transaction_product_download'] = false;
+                return false;
+            }   
+        }   
+        end( $GLOBALS['it_exchange']['transaction_product_downloads'] );
+        $GLOBALS['it_exchange']['transaction_product_download'] = false;
+        return false;
+	}
+
+	function product_download( $options=array() ) {
+		if ( ! empty( $options['has'] ) )
+			return (boolean) $this->_transaction_product_download;
+
+		if ( empty( $options['attribute'] ) )
+			return false;
+
+		if ( 'title' == $options['attribute'] || 'name' == $options['attribute'] )
+			$value = get_the_title( $this->_transaction_product_download['id'] );
+
+		return $value;
+	}
+
+	function product_download_hashes( $options=array() ) {
+		// Return false if we don't have a product id
+		if ( empty( $this->_transaction_product['product_id'] ) || empty( $this->_transaction_product_download ) )
+			return false;
+
+		// Return boolean if we'er just checking
+		if ( ! empty( $options['has'] ) )
+			return (boolean) it_exchange_get_download_hashes_for_transaction_product( $this->_transaction, $this->_transaction_product, $this->_transaction_product_download['id'] ); 
+
+		// If we made it here, we're doing a loop of transaction_product_download_hashes for the current query.
+        // This will init/reset the transaction_product_download_hashes global and loop through them.
+        if ( empty( $GLOBALS['it_exchange']['transaction_product_download_hashes'] ) ) { 
+            $GLOBALS['it_exchange']['transaction_product_download_hashes'] = it_exchange_get_download_hashes_for_transaction_product( $this->_transaction, $this->_transaction_product, $this->_transaction_product_download['id'] );
+            $GLOBALS['it_exchange']['transaction_product_download_hash'] = reset( $GLOBALS['it_exchange']['transaction_product_download_hashes'] );
+            return true;
+        } else {
+            if ( next( $GLOBALS['it_exchange']['transaction_product_download_hashes'] ) ) { 
+                $GLOBALS['it_exchange']['transaction_product_download_hash'] = current( $GLOBALS['it_exchange']['transaction_product_download_hashes'] );
+                return true;
+            } else {
+                $GLOBALS['it_exchange']['transaction_product_download_hash'] = false;
+                return false;
+            }   
+        }   
+        end( $GLOBALS['it_exchange']['transaction_product_download_hashes'] );
+        $GLOBALS['it_exchange']['transaction_product_download_hash'] = false;
+        return false;
+	}
+
+	function product_download_hash( $options=array() ) {
+		if ( ! empty( $options['has'] ) )
+			return (boolean) $this->_transaction_product_download_hash;
+
+		if ( ! isset( $options['attribute'] ) )
+			return false;
+
+		$defaults = array(
+			'date-format' => false,
+		);
+		$options = ITUtility::merge_defaults( $options, $defaults );
+
+		$hash_data = it_exchange_get_download_data_from_hash( $this->_transaction_product_download_hash );
+		if ( 'title' == $options['attribute'] || 'name' == $options['attribute'] )
+			$options['attribute'] = 'hash';
+		else if ( 'download-limit' == $options['attribute'] )
+			$options['attribute'] = 'download_limit';
+		else if ( 'download-count' == $options['attribute'] )
+			$options['attribute'] = 'downloads';
+
+		if ( 'expiration-date' == $options['attribute'] ) {
+			$date_format = empty( $options['date-format'] ) ? false : $options['date-format'];
+			$date = it_exchange_get_download_expiration_date_from_settings( $hash_data, $this->_transaction->post_date, $date_format );
+			$value = empty( $date ) ? false : $date;
+		} else if ( 'downloads-remaining' == $options['attribute'] ) {
+			$limit = empty( $hash_data['download_limit'] ) ? __( 'Unlimited Downloads', 'LION' ) : absint( $hash_data['download_limit'] );
+			$count = empty( $hash_data['downloads'] ) ? 0 : absint( $hash_data['downloads'] );
+			$remaining = ( $limit - $count );
+			$value = ( $remaining < 0 ) ? 0 : $remaining;
+		} else if ( 'download-url' == $options['attribute'] ) {
+			$value = add_query_arg( array( 'it-exchange-download' => $hash_data['hash'] ), get_home_url() );
+		} else {
+			$value = isset( $hash_data[$options['attribute']] ) ? $hash_data[$options['attribute']] : false;
+		}
+
+		return $value;
+	}
 }
--- lib/templates/content-confirmation.php
+++ lib/templates/content-confirmation.php
@@ -6,9 +6,56 @@
 		<?php it_exchange( 'transaction', 'instructions' ); ?><br />
 
 		<?php if ( it_exchange( 'transaction', 'has-products' ) ) : ?>
-			<?php while( it_exchange( 'transaction', 'products' ) ) : ?>
-				<?php it_exchange( 'product', 'title' ); ?>
-			<?php endwhile; ?>
+			<div class="transaction-products">
+				<?php while( it_exchange( 'transaction', 'products' ) ) : ?>
+					<div class="transaction-product">
+						<?php it_exchange( 'transaction', 'product-attribute', array( 'attribute' => 'title', 'wrap' => 'h3' ) ); ?>
+						<?php if ( it_exchange( 'transaction', 'has-product-downloads' ) ) : ?>
+							<div class="transaction-product-downloads">
+								<?php while( it_exchange( 'transaction', 'product-downloads' ) ) : ?>
+									<div class="transaction-product-download">
+										<div class="transaction-product-download-titile">
+											<?php it_exchange( 'transaction', 'product-download', array( 'attribute' => 'title' ) ); ?>
+										</div>
+										<?php if ( it_exchange( 'transaction', 'has-product-download-hashes' ) ) : ?>
+											<div class="transaction-product-download-hashes">
+												<?php while( it_exchange( 'transaction', 'product-download-hashes' ) ) : ?>
+													<div class="transaction-product-download-hash">
+														<div class="it-exchange-transaction-product-download-hash-hash">
+															<?php it_exchange( 'transaction', 'product-download-hash', array( 'attribute' => 'hash' ) ); ?>
+														</div>
+														<?php if ( it_exchange( 'transaction', 'get-product-download-hash', array( 'attribute' => 'expires' ) ) ) : ?>
+															<div class="it-exchange-transaction-product-download-hash-expiration-date">
+																<?php _e( 'Expires on', 'LION' ); ?> <?php it_exchange( 'transaction', 'product-download-hash', array( 'attribute' => 'expiration-date' ) ); ?>
+															</div>
+														<?php else : ?>
+															<div class="it-exchange-transaction-product-download-hash-expiration-date">
+																<?php _e( 'No expiration date', 'LION' ); ?>
+															</div>
+														<?php endif; ?>
+														<?php if ( it_exchange( 'transaction', 'get-product-download-hash', array( 'attribute' => 'download-limit' ) ) ) : ?>
+															<div class="it-exchange-transaction-product-download-hash-download-limit">
+																<?php it_exchange( 'transaction', 'product-download-hash', array( 'attribute' => 'downloads-remaining' ) ); ?> <?php _e( 'download(s) remaining', 'LION' ); ?> 
+															</div>
+														<?php else : ?>
+															<div class="it-exchange-transaction-product-download-hash-download-limit">
+																<?php _e( 'Unlimited downloads', 'LION' ); ?>
+															</div>
+														<?php endif; ?>
+														<?php if ( it_exchange( 'transaction', 'get-product-download-hash', array( 'attribute' => 'downloads-remaining' ) ) ) : ?>
+															<a href="<?php it_exchange( 'transaction', 'product-download-hash', array( 'attribute' => 'download-url' ) ); ?>"><?php _e( 'Download Now', 'LION' ); ?></a>
+														<?php endif; ?>
+													</div>
+												<?php endwhile; ?>
+											</div>
+										<?php endif; ?>
+									</div>
+								<?php endwhile; ?>
+							</div>
+						<?php endif; ?>
+					</div>
+				<?php endwhile; ?>
+			</div>
 		<?php endif; ?>
 	<?php endwhile; ?>
 <?php endif; ?>
