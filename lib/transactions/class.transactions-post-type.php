e9024390632ccbd64c626ebb6abaac7968ea3227
Glenn Ansley <glenn@ithemes.com>
2013-06-10 22:24:33 -0500

    Remove Slug metabox from Transaction Details.\n Remove Screen Options Tab from Transaction Details

--- lib/transactions/class.transactions-post-type.php
+++ lib/transactions/class.transactions-post-type.php
@@ -356,6 +356,8 @@ class IT_Exchange_Transaction_Post_Type {
 	function register_transaction_details_admin_metabox( $post ) {
 		// Remove Publish metabox
 		remove_meta_box( 'submitdiv', 'it_exchange_tran', 'side' );
+		remove_meta_box( 'slugdiv', 'it_exchange_tran', 'normal' );
+		add_filter('screen_options_show_screen', '__return_false');
 
 		// Transaction Details
 		$title     = __( 'Transaction Details', 'LION' );
