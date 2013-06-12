d98054810b1bdb812ffe78b96fe1023655c8e71c
Justin Kopepasah <justin@kopepasah.com>
2013-06-12 16:01:19 -0500

    Prettied up the downloads.

--- lib/templates/content-downloads.php
+++ lib/templates/content-downloads.php
@@ -22,36 +22,38 @@
 									<div class="download-info">
 										<h4><?php it_exchange( 'transaction', 'product-download', array( 'attribute' => 'title' ) ); ?></h4>
 										<?php if ( it_exchange( 'transaction', 'has-product-download-hashes' ) ) : ?>
-											<div class="transaction-product-download-hashes">
+											<ul class="transaction-product-download-hashes">
 												<?php while( it_exchange( 'transaction', 'product-download-hashes' ) ) : ?>
-													<div class="transaction-product-download-hash">
-														<div class="it-exchange-transaction-product-download-hash-hash">
+													<li class="transaction-product-download-hash">
+														<code class="download-hash">
 															<?php it_exchange( 'transaction', 'product-download-hash', array( 'attribute' => 'hash' ) ); ?>
-														</div>
+														</code>
 														<?php if ( it_exchange( 'transaction', 'get-product-download-hash', array( 'attribute' => 'expires' ) ) ) : ?>
-															<div class="it-exchange-transaction-product-download-hash-expiration-date">
+															<span class="download-expiration">
 																<?php _e( 'Expires on', 'LION' ); ?> <?php it_exchange( 'transaction', 'product-download-hash', array( 'attribute' => 'expiration-date' ) ); ?>
-															</div>
+															</span>
 														<?php else : ?>
-															<div class="it-exchange-transaction-product-download-hash-expiration-date">
+															<span class="download-expiration">
 																<?php _e( 'No expiration date', 'LION' ); ?>
-															</div>
+															</span>
 														<?php endif; ?>
 														<?php if ( it_exchange( 'transaction', 'get-product-download-hash', array( 'attribute' => 'download-limit' ) ) ) : ?>
-															<div class="it-exchange-transaction-product-download-hash-download-limit">
+															<span class="download-limit">
 																<?php it_exchange( 'transaction', 'product-download-hash', array( 'attribute' => 'downloads-remaining' ) ); ?> <?php _e( 'download(s) remaining', 'LION' ); ?>
-															</div>
+															</span>
 														<?php else : ?>
-															<div class="it-exchange-transaction-product-download-hash-download-limit">
+															<span class="download-limit">
 																<?php _e( 'Unlimited downloads', 'LION' ); ?>
-															</div>
+															</span>
 														<?php endif; ?>
 														<?php if ( it_exchange( 'transaction', 'get-product-download-hash', array( 'attribute' => 'downloads-remaining' ) ) ) : ?>
-															<a href="<?php it_exchange( 'transaction', 'product-download-hash', array( 'attribute' => 'download-url' ) ); ?>"><?php _e( 'Download Now', 'LION' ); ?></a>
+															<span>
+																<a href="<?php it_exchange( 'transaction', 'product-download-hash', array( 'attribute' => 'download-url' ) ); ?>"><?php _e( 'Download Now', 'LION' ); ?></a>
+															</span>
 														<?php endif; ?>
-													</div>
+													</li>
 												<?php endwhile; ?>
-											</div>
+											</ul>
 										<?php endif; ?>
 									</div>
 								</div>
