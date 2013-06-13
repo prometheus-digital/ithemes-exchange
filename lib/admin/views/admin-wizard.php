ec041e297c6e51d5c5ae54bcf9316c8ceeb6da6c
Justin Kopepasah <justin@kopepasah.com>
2013-06-13 11:16:27 -0500

    Fixed the button on the wizard screen.

--- lib/admin/views/admin-wizard.php
+++ lib/admin/views/admin-wizard.php
@@ -76,7 +76,7 @@
 				-->
 				
 				<div class="field submit-wrapper">
-					<?php $form->add_submit( 'submit', __( 'Start Selling!', 'LION' ) ); ?>
+					<?php $form->add_submit( 'submit', array( 'class' => 'button button-primary button-large', 'value' => __( 'Start Selling!', 'LION' ) ) ); ?>
 					<?php $form->add_hidden( 'dismiss-wizard-nag', true ); ?>
 					<?php $form->add_hidden( 'wizard-submitted', true ); ?>
 				</div>
