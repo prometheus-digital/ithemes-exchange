aa1477a401e9272dc732f6c6645b6ef4d60c4885
Glenn Ansley <glenn@ithemes.com>
2013-06-20 16:31:38 -0500

    Remove product(s) from cart summary and replace with product/products based on count

--- lib/templates/super-widget-product.php
+++ lib/templates/super-widget-product.php
@@ -1,8 +1,12 @@
 <?php if ( it_exchange( 'product', 'found' ) ) : ?>
 	<div class="it-exchange-sw-selecting">
-		<?php if ( it_exchange( 'cart', 'get-item-count' ) && it_exchange_is_multi_item_cart_allowed() ) : ?>
+		<?php if ( ( $count = it_exchange( 'cart', 'get-item-count' ) ) && it_exchange_is_multi_item_cart_allowed() ) : ?>
 			<div class="item-count">
-				<?php printf( __( 'You have %s item(s) in your <a href="%s">%s</a>', 'LION' ), it_exchange( 'cart', 'get-item-count' ), it_exchange_get_page_url( 'cart' ), strtolower( it_exchange_get_page_name( 'cart' ) ) ); ?>
+				<?php if ( $count === 1 ) : ?>
+					<?php printf( __( 'You have 1 item in your <a href="%s">%s</a>', 'LION' ), it_exchange_get_page_url( 'cart' ), strtolower( it_exchange_get_page_name( 'cart' ) ) ); ?>
+				<?php else : ?>
+					<?php printf( __( 'You have %s items in your <a href="%s">%s</a>', 'LION' ), $count, it_exchange_get_page_url( 'cart' ), strtolower( it_exchange_get_page_name( 'cart' ) ) ); ?>
+				<?php endif; ?>
 			</div>
 		<?php endif; ?>
 	
