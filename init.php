cd74b04a8e22b5ec9aa2ff0f247e4063fbc8a144
Glenn Ansley <glenn@ithemes.com>
2013-06-24 16:21:54 -0500

    Bumping version to 0.4.8. Udating history.txt

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange
- * Version: 0.4.7
+ * Version: 0.4.8
  * Description: Turns your WordPress site into a Lean, Mean Selling Machine!
  * Plugin URI: http://ithemes.com/purchase/ithemes-exchange/
  * Author: iThemes
@@ -24,7 +24,7 @@
 */
 class IT_Exchange {
 
-	var $_version         = '0.4.7';
+	var $_version         = '0.4.8';
 	var $_wp_minimum      = '3.5';
 	var $_slug            = 'it-exchange';
 	var $_name            = 'iThemes Exchange';
