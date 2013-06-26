b72c74f537376bdf2083c83394ae3820e6420b1d
Glenn Ansley <glenn@ithemes.com>
2013-06-26 14:50:49 -0500

    Remaining localization for JS

--- lib/admin/class.admin.php
+++ lib/admin/class.admin.php
@@ -1244,7 +1244,7 @@ Order: %s
 				);
 			} else if ( $_GET['tab'] == 'pages') {
 				wp_enqueue_script( 'it-exchange-settings-pages', ITUtility::get_url_from_file( dirname( __FILE__ ) ) . '/js/settings-pages.js' );
-				wp_localize_script( 'it-exchange-settings-pages', 'settingsGenearlL10n', array(
+				wp_localize_script( 'it-exchange-settings-pages', 'settingsPagesL10n', array(
 						'emptyWPPage'  => __( 'Please make sure all WordPress page types have a WordPress page selected.', 'LION' ),
 					)
 				);
