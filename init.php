88e3f1a6072887539847928a3f94f47d7894c108
Glenn Ansley <glenn@ithemes.com>
2013-06-28 08:48:32 -0500

    Modifying version var and history.txt after packaging bot did its thing

--- init.php
+++ init.php
@@ -24,7 +24,7 @@
 */
 class IT_Exchange {
 
-	var $_version         = '0.4.12';
+	var $_version         = '0.4.13';
 	var $_wp_minimum      = '3.5';
 	var $_slug            = 'it-exchange';
 	var $_name            = 'iThemes Exchange';
