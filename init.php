a0ce00c2e1dbf9f2bab27e06b7b66b5673a8e81d
Git User <git@petra.ithemes.com>
2013-06-28 08:46:23 -0500

    0.4.13: updated: lib/classes

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange
- * Version: 0.4.12
+ * Version: 0.4.13
  * Description: Turns your WordPress site into a Lean, Mean Selling Machine!
  * Plugin URI: http://ithemes.com/purchase/ithemes-exchange/
  * Author: iThemes
