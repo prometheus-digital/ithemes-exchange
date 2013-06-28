e705ae8ed28ff088847f68f4d8658b2d38c71d4e
Lew Ayotte <lew@lewayotte.com>
2013-06-28 10:53:57 -0400

    added processing dialog to stripe payment upon submit

---
+++ core-addons/transaction-methods/stripe/js/stripe-addon.js
@@ -0,0 +1,23 @@
+// JavaScript Document
+jQuery(document).ready(function($) {	
+	$( 'form#stripe_form').on('submit', function() {
+		var style = "position: fixed; " +
+					"display: none; " +
+					"z-index: 1000; " +
+					"top: 50%; " +
+					"left: 50%; " +
+					"background-color: #E8E8E8; " +
+					"border: 1px solid #555; " +
+					"padding: 15px; " +
+					"width: 500px; " +
+					"min-height: 80px; " +
+					"margin-left: -250px; " + 
+					"margin-top: -150px;" +
+					"text-align: center;" +
+					"vertical-align: middle;";
+		jQuery('body').append("<div id='results' style='" + style + "'></div>");
+		jQuery('#results').html("<p>" + stripeAddonL10n.processing_payment_text + "</p>" +
+								"<p><img src='/wp-includes/js/thickbox/loadingAnimation.gif' /></p>");
+		jQuery('#results').show();
+	});
+});
