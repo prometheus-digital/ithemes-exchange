5f20e95c1ed46b6c2ff6583ed3f11081adfc9b91
Justin Kopepasah <kopepasah@gmail.com>
2013-07-01 12:00:46 -0500

    Added processing payment notice when purchasing with Stripe.

--- core-addons/transaction-methods/stripe/js/stripe-addon.js
+++ core-addons/transaction-methods/stripe/js/stripe-addon.js
@@ -1,21 +1,7 @@
-// JavaScript Document
 function it_exchange_stripe_processing_payment_popup() {
-	var style = "position: fixed; " +
-				"display: none; " +
-				"z-index: 1000; " +
-				"top: 50%; " +
-				"left: 50%; " +
-				"background-color: #E8E8E8; " +
-				"border: 1px solid #555; " +
-				"padding: 15px; " +
-				"width: 500px; " +
-				"min-height: 80px; " +
-				"margin-left: -250px; " + 
-				"margin-top: -150px;" +
-				"text-align: center;" +
-				"vertical-align: middle;";
-	jQuery('body').append("<div id='results' style='" + style + "'></div>");
-	jQuery('#results').html("<p>" + stripeAddonL10n.processing_payment_text + "</p>" +
-							"<p><img src='/wp-includes/js/thickbox/loadingAnimation.gif' /></p>");
-	jQuery('#results').show();
+	jQuery( document ).ready( function( $ ) {
+		var it_exchange_stripe_processing_html = '<div id="it-exchange-processing-payment"><div><span></span><p>' + stripeAddonL10n.processing_payment_text + '</p></div></div>';
+		$( 'body' ).append( it_exchange_stripe_processing_html );
+		$( '#it-exchange-processing-payment' ).show();
+	});
 }
