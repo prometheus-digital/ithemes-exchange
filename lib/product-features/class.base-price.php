8e9dbb49427d95b69f3729918f17be7f163a719d
Justin Kopepasah <kopepasah@gmail.com>
2013-07-05 14:24:17 -0500

    Convert the currency to a number based format on focus out of the input field (Code freeze? What's that?). :)

--- lib/product-features/class.base-price.php
+++ lib/product-features/class.base-price.php
@@ -116,10 +116,13 @@ class IT_Exchange_Base_Price {
 		$description = __( 'Price', 'LION' );
 		$description = apply_filters( 'it_exchange_base-price_addon_metabox_description', $description );
 
+		$settings = it_exchange_get_option( 'settings_general' );
+		$currency = it_exchange_get_currency_symbol( $settings['default-currency'] ) 
+
 		// Echo the form field
 		?>
 			<label for="base-price"><?php esc_html_e( $description ); ?></label>
-			<input type="text" placeholder="<?php esc_attr_e( it_exchange_format_price( '0.00' ) ); ?>" id="base-price" name="it-exchange-base-price" value="<?php esc_attr_e( $product_base_price ); ?>" tabindex="2" />
+			<input type="text" placeholder="<?php esc_attr_e( it_exchange_format_price( '0.00' ) ); ?>" id="base-price" name="it-exchange-base-price" value="<?php esc_attr_e( $product_base_price ); ?>" tabindex="2" data-symbol="<?php esc_attr_e( $currency['symbol'] ); ?>" data-symbol-position="<?php esc_attr_e( $settings['currency-symbol-position'] ); ?>" data-thousands-separator="<?php esc_attr_e( $settings['currency-thousands-separator'] ); ?>" data-decimals-separator="<?php esc_attr_e( $settings['currency-decimals-separator'] ); ?>" />
 		<?php
 	}
 
