caf6b04b87081c9f400ac95c34ad95753cb33c80
Chris Jean <gaarai@gaarai.com>
2013-07-05 11:05:39 -0500

    Fix for escaping issues with blogname when setting default value for company-name.

--- lib/admin/class.admin.php
+++ lib/admin/class.admin.php
@@ -597,7 +597,7 @@ class IT_Exchange_Admin {
 			'currency-decimals-separator'  => '.',
 			'site-registration'            => 'it',
 			'company-email'                => get_bloginfo( 'admin_email' ),
-			'company-name'                 => esc_attr( get_bloginfo( 'name' ) ),
+			'company-name'                 => wp_specialchars_decode( get_bloginfo( 'name' ), ENT_QUOTES ),
 		);
 		$values = ITUtility::merge_defaults( $values, $defaults );
 		return $values;
