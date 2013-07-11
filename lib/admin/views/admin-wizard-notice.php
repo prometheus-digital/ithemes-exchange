1abfdc2b647f3773df9e0e99ca72148054b0c895
Justin Kopepasah <kopepasah@gmail.com>
2013-07-11 11:25:35 -0500

    Updated the nags (not to be confused with notices) formats and styles.
    The nags needed to be styled differently that standard noticed in order to differentiate the two. Also, a standardization was in order.

--- lib/admin/views/admin-wizard-notice.php
+++ lib/admin/views/admin-wizard-notice.php
@@ -6,74 +6,11 @@
 */
 // Just adding internal CSS rule here since it won't be around long.
 ?>
-<style type="text/css">
-	.it-exchange-wizard-notice {
-		background: #f3fce6 url('<?php echo ITUtility::get_url_from_file( dirname( dirname( __FILE__ ) ) . '/images/open-e.png' );?>') .85em center no-repeat;
-		border: 1px solid #D8E9C1;
-		border-bottom-width: 2px;
-		border-radius: 3px;
-		font-size: 14px;
-		line-height: 1.6;
-		padding: 1em .75em 1em 4em;
-		margin: 2.5em 1em 1em 0;
-		color: #44654e;
-	}
-	.it-exchange-wizard-notice.no-screen-options {
-		margin-top: 1em;
-	}
-	.it-exchange-wizard-notice a {
-		background: #D8E9C1;
-		color: #44654e;
-		font-weight: bold;
-		border: 1px solid #b7cc9b;
-		border-bottom: 2px solid #b7cc9b;
-		border-radius: 3px;
-		padding: 6px 14px;
-		-webkit-transition:  all .1s linear;
-		-moz-transition:  all .1s linear 0s;
-		text-decoration: none;
-		position: absolute;
-		margin: -8px 0 0 8px;
-	}
-	.it-exchange-wizard-notice a:hover, .it-exchange-wizard-notice button:hover {
-		background: #f9fff0;
-	}
-	.it-exchange-wizard-notice button:active {
-		background: #f3fce6;
-	}
-	.it-exchange-wizard-notice a:active {
-		background: #f3fce6;
-		border-bottom-width: 1px;
-		margin-top: -7px;
-		box-shadow: inset 0 2px 5px -3px rgba(0, 0, 0, 0.5);	
-	}
-	.it-exchange-wizard-notice button {
-		background: #f3fce6;
-		border: 1px solid #D8E9C1;
-		border-bottom: 2px solid #D8E9C1;
-		border-radius: 3px;
-		color: #44654e;
-		font-weight: bold;
-		padding: 5px 10px;
-		margin: -4px 0 0 0;
-		float: right;
-		-webkit-transition:  all .1s linear;
-		-moz-transition:  all .1s linear 0s;
-	}
-	.it-exchange-wizard-notice a.dismiss {
-		right: 22px;
-	}
-</style>
-<div class="it-exchange-wizard-notice">
+<div id="it-exchange-wizard-nag" class="it-exchange-nag">
 	<?php
 	$wizard_link    = add_query_arg( array( 'page' => 'it-exchange-setup' ), admin_url( 'admin.php' ) );
 	$wizard_dismiss = add_query_arg( array( 'it_exchange_settings-dismiss-wizard-nag' => true ) );
-	echo __( 'iThemes Exchange is now installed.', 'LION' ) . ' <a href="' . $wizard_link . '">' . __( 'Go to Quick Setup', 'LION' ) . '</a>';
+	echo __( 'iThemes Exchange is now installed.', 'LION' ) . ' <a class="btn" href="' . $wizard_link . '">' . __( 'Go to Quick Setup', 'LION' ) . '</a>';
 	?>
-	<a class="dismiss" href="<?php esc_attr_e( $wizard_dismiss ); ?>">&times;</a>
+	<a class="dismiss btn" href="<?php esc_attr_e( $wizard_dismiss ); ?>">&times;</a>
 </div>
-<script type="text/javascript">
-	if ( ! jQuery('#screen-options-wrap').length ) {
-		jQuery('.it-exchange-wizard-notice').addClass('no-screen-options');
-	}
-</script>
