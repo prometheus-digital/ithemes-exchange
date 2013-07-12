de2a579f96f8826f1a7cfb8202220632bb9b7868
Glenn Ansley <glenn@ithemes.com>
2013-07-12 16:18:22 -0500

    Add hooks to /templates/cart/items/details/ files

--- lib/templates/cart/items/details/quantity.php
+++ lib/templates/cart/items/details/quantity.php
@@ -1,8 +1,17 @@
-
-<!-- We could also add do action start here. -->
+<?php
+/**
+ * This is the default template for the Quantity cart item details in the content-cart.php template part
+ * @since 1.1.0
+ * @version 1.1.0
+ * @package IT_Exchange
+*/
+?>
+<?php do_action( 'it_exchange_cart_item_details_before_quantity' ); ?>
 <div class="it-exchange-cart-item-quantity it-exchange-table-column">
+	<?php do_action( 'it_exchange_cart_item_details_begin_quantity' ); ?>
 	<div class="it-exchange-table-column-inner">
 		<?php it_exchange( 'cart-item', 'quantity' ) ?>
 	</div>
+	<?php do_action( 'it_exchange_cart_item_details_end_quantity' ); ?>
 </div>
-<!-- We could also add do action end here. -->
+<?php do_action( 'it_exchange_cart_item_details_after_quantity' ); ?>
--- lib/templates/cart/items/details/title.php
+++ lib/templates/cart/items/details/title.php
@@ -1,8 +1,17 @@
-
-<!-- We could also add do action start here. -->
+<?php
+/**
+ * The main template file for the Title detail in the cart-items loop for content-cart template part
+ * @since 1.1.0
+ * @version 1.1.0
+ * @package IT_Exchange
+*/
+?>
+<?php do_action( 'it_exchange_cart_item_details_before_title' ); ?>
 <div class="it-exchange-cart-item-title it-exchange-table-column">
+	<?php do_action( 'it_exchange_cart_item_details_begin_title' ); ?>
 	<div class="it-exchange-table-column-inner">
 		<a href="<?php it_exchange( 'cart-item', 'permalink' ) ?>"><?php it_exchange( 'cart-item', 'title' ) ?></a>
 	</div>
+	<?php do_action( 'it_exchange_cart_item_details_end_title' ); ?>
 </div>
-<!-- We could also add do action end here. -->
+<?php do_action( 'it_exchange_cart_item_details_after_title' ); ?>
