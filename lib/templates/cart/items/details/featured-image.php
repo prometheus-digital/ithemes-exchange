449bcf6e7f911b2a9b27a8a7d82cbf5b688555ec
Glenn Ansley <glenn@ithemes.com>
2013-07-12 16:18:22 -0500

    Add hooks to /templates/cart/items/details/ files

--- lib/templates/cart/items/details/featured-image.php
+++ lib/templates/cart/items/details/featured-image.php
@@ -1,8 +1,17 @@
-
-<!-- We could also add do action start here. -->
+<?php
+/**
+ * The main template file for the Featured Image detail in the cart-items loop for content-cart template part
+ * @since 1.1.0
+ * @version 1.1.0
+ * @package IT_Exchange
+*/
+?>
+<?php do_action( 'it_exchange_cart_item_details_before_featured_image' ); ?>
 <div class="it-exchange-cart-item-thumbnail it-exchange-table-column">
+	<?php do_action( 'it_exchange_cart_item_details_begin_featured_image' ); ?>
 	<div class="it-exchange-table-column-inner">
 		<?php it_exchange( 'cart-item', 'featured-image' ); ?>
 	</div>
+	<?php do_action( 'it_exchange_cart_item_details_end_featured_image' ); ?>
 </div>
-<!-- We could also add do action end here. -->
+<?php do_action( 'it_exchange_cart_item_details_after_featured_image' ); ?>
