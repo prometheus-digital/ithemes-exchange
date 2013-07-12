72c01a0f98a0727663cd5a916ff04eba25140e67
Glenn Ansley <glenn@ithemes.com>
2013-07-12 10:52:05 -0500

    Upated content-cart.php.\n Added lib/templates/cart/ and multiple template parts.\n Added calls to /api/cart.php to filter cart-item rows, cart-totals columns/rows, cart action.

---
+++ lib/templates/cart/cart-item-columns/subtotal.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Subtotal column for cart-items
+*/
+?>
+<div class="cart-item-subtotal cart-column">
+	<div class="cart-column-inner">
+		<?php it_exchange( 'cart-item', 'subtotal' ); ?>
+	</div>
+</div>
