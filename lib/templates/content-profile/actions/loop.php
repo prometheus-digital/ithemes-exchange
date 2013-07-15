59c865f5eb285d85c84d46cb2103f38b43c616ee
Lew Ayotte <lew@lewayotte.com>
2013-07-15 15:45:05 -0400

    redux of registration and profile actionns loops

--- lib/templates/content-profile/actions/loop.php
+++ lib/templates/content-profile/actions/loop.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * This is the default template part for the
- * coupon details loop in the content-profile
+ * coupon actions loop in the content-profile
  * template part.
  *
  * @since 1.1.0
@@ -15,18 +15,18 @@
 */
 ?>
 
-<?php do_action( 'it_exchange_content_profile_fields_before_loop' ); ?>
-	<?php do_action( 'it_exchange_content_profile_fields_begin_loop' ); ?>
-	<div class="customer-info">
-		<?php foreach ( it_exchange_get_content_profile_field_details() as $detail ) : ?>
+<?php do_action( 'it_exchange_content_profile_actions_before_loop' ); ?>
+	<?php do_action( 'it_exchange_content_profile_actions_begin_loop' ); ?>
+	<div class="customer-actions">
+		<?php foreach ( it_exchange_get_content_profile_actions() as $detail ) : ?>
 			<?php
 			/** 
 			 * Theme and add-on devs should add code to this loop by 
 			 * hooking into it_exchange_get_content_profile_fields_details filter
 			 * and adding the appropriate template file to their theme or add-on
 			*/
-			it_exchange_get_template_part( 'content-profile/fields/details/' . $detail ); ?>
+			it_exchange_get_template_part( 'content-profile/actions/details/' . $detail ); ?>
 		<?php endforeach; ?>
 	</div>
-	<?php do_action( 'it_exchange_content_profile_fields_end_loop' ); ?>
-<?php do_action( 'it_exchange_content_profile_fields_after_loop' ); ?>
+	<?php do_action( 'it_exchange_content_profile_actions_end_loop' ); ?>
+<?php do_action( 'it_exchange_content_profile_actions_after_loop' ); ?>
