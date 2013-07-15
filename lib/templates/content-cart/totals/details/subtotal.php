f5510ae2b1190178819f9cf23117d08652130fae
Justin Kopepasah <kopepasah@gmail.com>
2013-07-15 15:04:16 -0500

    Added the detail class to the row instead of each of the columns and changed -title to -total on the total class.

--- lib/templates/content-cart/totals/details/subtotal.php
+++ lib/templates/content-cart/totals/details/subtotal.php
@@ -16,12 +16,12 @@
 ?>
 
 <?php do_action( 'it_exchange_content_cart_totals_details_before_subtotal' ); ?>
-<div class="it-exchange-cart-totals-title it-exchange-cart-subtotal it-exchange-table-column">
+<div class="it-exchange-cart-totals-title it-exchange-table-column">
 	<div class="it-exchange-table-column-inner">
 		<?php _e( 'Subtotal', 'LION' ); ?>
 	</div>
 </div>
-<div class="it-exchange-cart-totals-title it-exchange-cart-subtotal it-exchange-table-column">
+<div class="it-exchange-cart-totals-total it-exchange-table-column">
 	<div class="it-exchange-table-column-inner">
 		<?php it_exchange( 'cart', 'subtotal' ) ?>
 	</div>
