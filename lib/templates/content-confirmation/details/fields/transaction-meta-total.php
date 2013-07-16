0a1d757e24160877a3d9afcc045ff72c8370fa17
Justin Kopepasah <kopepasah@gmail.com>
2013-07-16 15:20:33 -0500

    Cleaned up the content confirmation.

--- lib/templates/content-confirmation/details/fields/transaction-meta-total.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * This is the default template part for the TOTAL in the transaction meta area of the Transaction Confirmation template part
- * @since 1.1.0
- * @version 1.1.0
- * @package IT_Exchange
-*/
-?>
-<?php it_exchange( 'transaction', 'total' ); ?><br />
