0beafd95863964fc6ff83328fe263e0d3d8e718c
Glenn Ansley <glenn@ithemes.com>
2013-07-16 14:36:30 -0500

    Revamping some hooks in content-downloads

--- lib/templates/content-downloads/loops/download-info.php
+++ lib/templates/content-downloads/loops/download-info.php
@@ -8,21 +8,23 @@
 */
 ?>
 <?php if ( it_exchange( 'transaction', 'has-product-downloads' ) ) : ?>
-<?php do_action( 'it_exchange_content_downloads_before_download_info_loop' ); ?>
+	<?php do_action( 'it_exchange_content_downloads_before_downloads_wrapper' ); ?>
 	<div class="downloads-wrapper">
-	<?php do_action( 'it_exchange_content_downloads_fields_begin_download_info_loop' ); ?>
+		<?php do_action( 'it_exchange_content_downloads_before_downloads_loop' ); ?>
 		<?php while ( it_exchange( 'transaction', 'product-downloads' ) ) : ?>
+			<?php do_action( 'it_exchange_content_downloads_begin_downloads_loop' ); ?>
 			<div class="download">
 				<?php it_exchange_get_template_part( 'content-downloads/details/fields/confirmation-url' ); ?>
 				<div class="download-info">
                     <?php it_exchange_get_template_part( 'content-downloads/details/fields/download-title' ); ?>
 					<?php if ( it_exchange( 'transaction', 'has-product-download-hashes' ) ) : ?>
-                    <?php it_exchange_get_template_part( 'content-downloads/loops/download-hashes' ); ?>
+						<?php it_exchange_get_template_part( 'content-downloads/loops/download-hashes' ); ?>
 					<?php endif; ?>
 				</div>
 			</div>
+			<?php do_action( 'it_exchange_content_downloads_end_downloads_loop' ); ?>
 		<?php endwhile; ?>
-   	<?php do_action( 'it_exchange_content_downloads_fields_end_download_info_loop' ); ?>
+		<?php do_action( 'it_exchange_content_downloads_after_downloads_loop' ); ?>
 	</div>
-<?php do_action( 'it_exchange_content_downloads_fields_after_download_info_loop' ); ?>
+	<?php do_action( 'it_exchange_content_downloads_after_downloads_wrapper' ); ?>
 <?php endif; ?>
