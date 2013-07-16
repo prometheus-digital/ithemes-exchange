4f806e5f964746596ea8edb21370aa6bd2286692
Glenn Ansley <glenn@ithemes.com>
2013-07-16 15:27:03 -0500

    Break out super widget product

--- lib/templates/super-widget-product.php
+++ lib/templates/super-widget-product.php
@@ -19,15 +19,19 @@
 <?php it_exchange_get_template_part( 'messages' ); ?>
 
 <?php if ( it_exchange( 'product', 'found' ) ) : ?>
+	<?php do_action( 'it_exchange_super_widget_product_before_product' ); ?>
 	<div class="it-exchange-sw-product it-exchange-sw-processing-product">
+		<?php do_action( 'it_exchange_super_widget_product_begin_product' ); ?>
+
 		<?php if ( it_exchange_is_multi_item_cart_allowed() ) : ?>
 			<?php it_exchange_get_template_part( 'super-widget', 'cart' ); ?>
 		<?php endif; ?>
 		
 		<?php if ( it_exchange_is_page( 'product' ) && ( !it_exchange_is_multi_item_cart_allowed() || !it_exchange_is_current_product_in_cart() ) ) : ?>
-			<div class="purchase-options">
-				<?php it_exchange( 'product', 'purchase-options', array( 'add-to-cart-edit-quantity' => false, 'buy-now-edit-quantity' => false ) ); ?>
-			</div>
+			<?php it_exchange_get_template_part( 'super-widget-product/purchase-options' ); ?>
 		<?php endif; ?>
+
+		<?php do_action( 'it_exchange_super_widget_product_end_product' ); ?>
 	</div>
+	<?php do_action( 'it_exchange_super_widget_product_after_product' ); ?>
 <?php endif; ?>
