da7244fb0292d101d780a3218b70f5b3cf0805ae
Justin Kopepasah <kopepasah@gmail.com>
2013-07-16 15:20:33 -0500

    Cleaned up the content confirmation.

--- lib/templates/content-confirmation.php
+++ lib/templates/content-confirmation.php
@@ -12,12 +12,11 @@
  * this template in a theme, simply copy over this
  * file's content to the exchange directory located
  * at your templates root.
- * 
- * Example: theme/exchange/content-confirmation.php
 */
 ?>
+
 <?php do_action( 'it_exchange_content_confirmation_before_wrap' ); ?>
-<div id="it-exchange-wrap it-exchange-confirmation">
-<?php it_exchange_get_template_part( 'content-confirmation/loops/transactions' ); ?>
+<div id="it-exchange-confirmation" class="it-exchange-wrap">
+	<?php it_exchange_get_template_part( 'content-confirmation/loops/transactions' ); ?>
 </div>
 <?php do_action( 'it_exchange_content_confirmation_after_wrap' ); ?>
