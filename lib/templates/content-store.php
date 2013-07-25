b46517055383f45a93fbea570fdf74e780455388
Justin Kopepasah <kopepasah@gmail.com>
2013-07-25 14:56:11 -0500

    Bug fix and other things for the content-store.

--- lib/templates/content-store.php
+++ lib/templates/content-store.php
@@ -18,7 +18,7 @@
 <div id="it-exchange-store" class="it-exchange-wrap it-exchange-account">
 	<?php do_action( 'it_exchange_content_store_begin_wrap' ); ?>
 	<?php it_exchange_get_template_part( 'messages' ); ?>
-    <?php it_exchange_get_template_part( 'content-store/loops/products' ); ?>
+	<?php it_exchange_get_template_part( 'content-store/loops/products' ); ?>
 	<?php do_action( 'it_exchange_content_store_after_wrap' ); ?>
 </div>
 <?php do_action( 'it_exchange_content_store_after_wrap' ); ?>
