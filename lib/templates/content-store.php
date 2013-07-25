2b3165c8f53d087ca189ed2d1901510742395d7e
Justin Kopepasah <kopepasah@gmail.com>
2013-07-25 14:44:59 -0500

    Moved messages in content-store.

--- lib/templates/content-store.php
+++ lib/templates/content-store.php
@@ -14,11 +14,10 @@
 */
 ?>
 
-<?php it_exchange_get_template_part( 'messages' ); ?>
-
 <?php do_action( 'it_exchange_content_store_before_wrap' ); ?>
 <div id="it-exchange-store" class="it-exchange-wrap it-exchange-account">
 	<?php do_action( 'it_exchange_content_store_begin_wrap' ); ?>
+	<?php it_exchange_get_template_part( 'messages' ); ?>
     <?php it_exchange_get_template_part( 'content-store/loops/products' ); ?>
 	<?php do_action( 'it_exchange_content_store_after_wrap' ); ?>
 </div>
