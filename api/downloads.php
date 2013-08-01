c005b9a82adb6c5c77e5fd06d15aea80a0b9a46c
Glenn Ansley <glenn@ithemes.com>
2013-08-01 10:01:39 -0500

    Revert "Fix bug preventing large files from being downloaded" This reverts commit a5d1c72298f7c217cc7cf0486247fe2e18cb1457.

--- api/downloads.php
+++ api/downloads.php
@@ -276,7 +276,7 @@ function it_exchange_serve_product_download( $hash_data ) {
 
 	// Attempt to grab file
 	$filename = basename( $url );
-	if ( $response = wp_remote_get( $url, array( 'stream' => true ) ) ) {
+	if ( $response = wp_remote_get( $url ) ) {
 		if ( ! is_wp_error( $response ) ) {
 			$valid_response_codes = array(
 				200,
@@ -306,10 +306,6 @@ function it_exchange_serve_product_download( $hash_data ) {
 
 				// Force download
 				header( 'Content-disposition: attachment; filename="' . $filename . '"' );
-
-				// Flush buffers
-				ob_end_clean();
-				flush();
 
 				// Deliver file
 				echo wp_remote_retrieve_body( $response );
