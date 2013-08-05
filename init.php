986fe6030f38f7fab7414b63b1835dbaf71d1d2c
Git User <git@petra.ithemes.com>
2013-08-05 11:41:22 -0500

    1.1.1: updated: lib/classes

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange
- * Version: 1.1.0
+ * Version: 1.1.1
  * Description: Easily sell your digital goods with iThemes Exchange, simple ecommerce for WordPress
  * Plugin URI: http://ithemes.com/exchange/
  * Author: iThemes
