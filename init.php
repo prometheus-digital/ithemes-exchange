7e98466fc84f89c92766523f531e220ff2dc4883
Glenn Ansley <glenn@ithemes.com>
2013-09-16 14:22:51 -0500

    Tagging 1.3.0

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange
- * Version: 1.2.1
+ * Version: 1.3.0
  * Description: Easily sell your digital goods with iThemes Exchange, simple ecommerce for WordPress
  * Plugin URI: http://ithemes.com/exchange/
  * Author: iThemes
@@ -23,7 +23,7 @@
 */
 class IT_Exchange {
 
-	var $_version         = '1.2.1';
+	var $_version         = '1.3.0';
 	var $_wp_minimum      = '3.5';
 	var $_slug            = 'it-exchange';
 	var $_name            = 'iThemes Exchange';
