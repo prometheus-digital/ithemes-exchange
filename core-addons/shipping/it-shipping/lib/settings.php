ba90decdbae9323334f0b651b3bf474120a114d3
Glenn Ansley <glenn@ithemes.com>
2013-09-19 12:50:02 -0500

    Use new admin settings form class with Exchange shipping providers page.

--- core-addons/shipping/it-shipping/lib/settings.php
+++ core-addons/shipping/it-shipping/lib/settings.php
@@ -7,7 +7,7 @@
  * It will be intercepted though if the user clicks on it and redirected to 
  * The Exchange settings --> shipping tab.
  *
- * @since 1.0.0
+ * @since CHANGEME
  *
  * @return void
 */
@@ -24,39 +24,57 @@ function it_exchange_shipping_settings_callback() {
 	<?php
 }
 
+/**
+ * Prints the Shipping tab on the Exchange Settings admin page
+ *
+ * @since CHANGEME
+ *
+ * @param  string $current_tab the current tab being requested
+ * @return void
+*/
 function it_exchange_addon_shipping_print_settings_tab_link( $current_tab ) {
 	$active = 'shipping' == $current_tab ? 'nav-tab-active' : '';
 	?><a class="nav-tab <?php echo $active; ?>" href="<?php echo admin_url( 'admin.php?page=it-exchange-settings&tab=shipping' ); ?>"><?php _e( 'Shipping', 'LION' ); ?></a><?php
 }
 add_action( 'it_exchange_print_general_settings_tab_links', 'it_exchange_addon_shipping_print_settings_tab_link' );
 
+/**
+ * Prints the contents of the Shipping Tab
+ *
+ * First looks to see if a registered shipping provider's settins are being requested
+ * If so, it inits those fields.
+ * If not, it loads the general shipping settings
+ *
+ * @since CHANGEME
+ *
+ * @return void
+*/
 function it_exchange_addon_shipping_print_settings_tab() {
 	$settings = it_exchange_get_option( 'addon_shipping', true );
 
-	if ( it_exchange_has_messages( 'notice' ) ) {
-		foreach( (array) it_exchange_get_messages( 'notice' ) as $message ) {
-			ITUtility::show_status_message( $message );
-		}
-	}
-	if ( it_exchange_has_messages( 'error' ) ) {
-		foreach( (array) it_exchange_get_messages( 'error' ) as $message ) {
-			ITUtility::show_error_message( $message );
-		}
-	}
 	?>
 	<div class="wrap">
 
 		<?php $GLOBALS['IT_Exchange_Admin']->print_general_settings_tabs(); ?>
 
-		<?php it_exchange_print_shipping_provider_settings_tabs(); ?>
+		<?php 
+		// Print shipping provider options
+		it_exchange_print_shipping_provider_settings_tabs();
+		?>
 
 		<?php 
+		// Print specific provider settings page if being requested
 		$provider_settings_printed = false;
 		if ( ! empty( $_GET['provider'] ) && it_exchange_is_shipping_provider_registered( $_GET['provider'] ) ) {
-			include_once( dirname( __FILE__ ) . '/class-provider-settings-page.php' );
-			$provider_settings = new IT_Exchange_Provider_Settings_Page( $_GET['provider'] );
-			if ( is_callable( array( $provider_settings, 'print_settings_page' ) ) ) {
-				$provider_settings->print_settings_page();
+			if ( $provider = it_exchange_get_shipping_provider( $_GET['provider'] ) ) {
+				$options = array(
+					'prefix'       => 'addon-shipping-' . $provider->slug,
+					'form-options' => array(
+						'action' => add_query_arg( array( 'page' => 'it-exchange-settings', 'tab' => 'shipping', 'provider' => $provider->slug ), admin_url( 'admin' ) ),
+					),
+					'form-fields'  => $provider->provider_settings,
+				);
+				it_exchange_print_admin_settings_form( $options );
 				$provider_settings_printed = true;
 			}
 		}
@@ -64,6 +82,7 @@ function it_exchange_addon_shipping_print_settings_tab() {
 
 		<?php if ( ! $provider_settings_printed ) : ?>
 			<?php
+			// Print default settings page if not provider was requested
 			$options = array(
 				'prefix'       => 'addon-shipping-general',
 				'form-options' => array(
@@ -156,7 +175,7 @@ function it_exchange_addon_shipping_print_settings_tab() {
  *
  * I hate that this was setup like this. Seems like an uneeded function
  *
- * @since 1.0.0
+ * @since CHANGEME
  *
  * @return string the callback
 */
