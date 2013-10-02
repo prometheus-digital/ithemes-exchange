20734ae58e90ce44d7ae90d5791b2bc60da1af35
Glenn Ansley <glenn@ithemes.com>
2013-10-02 07:45:00 -0500

    Remove membership from Builder views.\n Register Cart and Checkout views based on mulit-item-cart status

--- lib/integrations/builder/functions.php
+++ lib/integrations/builder/functions.php
@@ -1,16 +1,16 @@
 <?php
 /**
  * Functions for integration with Builder.
- * 
+ *
  * @package IT_Exchange
 */
 
 /**
  * This unsets the views added by Exchange's custom
  * post types.
- * 
+ *
  * @package IT_Exchange
- * @since 1.3.3
+ * @since CHANGEME
  * @author Justin Kopepasah
  * @var $views
 */
@@ -20,79 +20,159 @@ function it_exchange_remove_default_exchange_builder_views( $views ) {
 			unset( $views[$view] );
 		}
 	}
-	
+
 	return $views;
 }
 add_filter( 'builder_get_available_views', 'it_exchange_remove_default_exchange_builder_views', 100 );
 
 /**
- * Set up the callbacks for the specific views.
- * 
+ * Builder theme callback to determine if this a product view
+ *
  * @package IT_Exchange
- * @since 1.3.3
+ * @since CHANGEME
  * @author Justin Kopepasah
 */
 function it_exchange_is_product_builder_view() {
     return it_exchange_is_page( 'product' );
 }
 
+/**
+ * Builder theme callback to determine if this a storeview
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_store_builder_view() {
     return it_exchange_is_page( 'store' );
 }
 
+/**
+ * Builder theme callback to determine if this a transaction view
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_transaction_builder_view() {
     return it_exchange_is_page( 'transaction' );
 }
 
+/**
+ * Builder theme callback to determine if this a registration view
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_registration_builder_view() {
     return it_exchange_is_page( 'registration' );
 }
 
+/**
+ * Builder theme callback to determine if this a account view
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_account_builder_view() {
     return it_exchange_is_page( 'account' );
 }
 
+/**
+ * Builder theme callback to determine if this a profile view
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_profile_builder_view() {
     return it_exchange_is_page( 'profile' );
 }
 
+/**
+ * Builder theme callback to determine if this a downloads view
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_downloads_builder_view() {
     return it_exchange_is_page( 'downloads' );
 }
 
+/**
+ * Builder theme callback to determine if this a purchases view
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_purchases_builder_view() {
     return it_exchange_is_page( 'purchases' );
 }
 
+/**
+ * Builder theme callback to determine if this a login view
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_login_builder_view() {
     return it_exchange_is_page( 'login' );
 }
 
+/**
+ * Builder theme callback to determine if this a logout view
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_logout_builder_view() {
     return it_exchange_is_page( 'logout' );
 }
 
+/**
+ * Builder theme callback to determine if this a confirmation view
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_confirmation_builder_view() {
     return it_exchange_is_page( 'confirmation' );
 }
 
-function it_exchange_is_memberships_builder_view() {
-    return it_exchange_is_page( 'memberships' );
-}
-
+/**
+ * Builder theme callback to determine if this a cart view
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_cart_builder_view() {
     return it_exchange_is_page( 'cart' );
 }
 
+/**
+ * Builder theme callback to determine if this a checkout view
+ *
+ * @package IT_Exchange
+ * @since CHANGEME
+ * @author Justin Kopepasah
+*/
 function it_exchange_is_checkout_builder_view() {
     return it_exchange_is_page( 'checkout' );
 }
 
 /**
  * Add the views to Builder's list of available views.
- * 
+ *
  * @package IT_Exchange
- * @since 1.3.3
+ * @since CHANGEME
  * @author Justin Kopepasah
  * @var $views
 */
@@ -153,11 +233,9 @@ function it_exchange_add_new_builder_views( $views ) {
 			'priority'    => '20',
 			'description' => __( 'The Exchange purchase confirmation page.', 'LION' ),
 		),
-		'it_exchange_is_memberships_builder_view' => array(
-			'name'        => _x( 'Exchange - Memberships', 'view', 'LION' ),
-			'priority'    => '20',
-			'description' => __( 'The Exchange memberships page.', 'LION' ),
-		),
+	);
+
+	$multi_item_views = array(
 		'it_exchange_is_cart_builder_view' => array(
 			'name'        => _x( 'Exchange - Cart', 'view', 'LION' ),
 			'priority'    => '20',
@@ -169,9 +247,12 @@ function it_exchange_add_new_builder_views( $views ) {
 			'description' => __( 'The Exchange checkout page.', 'LION' ),
 		),
 	);
-	
+
 	$views = array_merge( $views, $exchange_views );
-	
+
+	if ( it_exchange_is_multi_item_cart_allowed() )
+		$views = array_merge( $multi_item_views, $views );
+
     return $views;
 }
 add_filter( 'builder_get_available_views', 'it_exchange_add_new_builder_views', 100 );
--- lib/integrations/builder/init.php
+++ lib/integrations/builder/init.php
@@ -1,11 +1,10 @@
 <?php
 /**
  * Custom integrations with Builder.
- * 
+ *
  * @package IT_Exchange
- * @since 1.3.3
+ * @since CHANGEME
 */
-
 function it_exchange_builder_integration_setup() {
 	if ( isset( $GLOBALS['theme_index'] ) && 'it-builder' == $GLOBALS['theme_index'] ) {
 		require_once( dirname( __FILE__ ) . '/functions.php' );
