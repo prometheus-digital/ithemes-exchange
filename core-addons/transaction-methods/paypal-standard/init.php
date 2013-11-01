1337f1147e63edac983433f7b043b456fb9b11e1
Lew Ayotte <lew@lewayotte.com>
2013-11-01 10:32:12 -0400

    fixing paypal standar return code

--- core-addons/transaction-methods/paypal-standard/init.php
+++ core-addons/transaction-methods/paypal-standard/init.php
@@ -389,7 +389,7 @@ function it_exchange_paypal_standard_addon_get_payment_url( $temp_id ) {
 			'business'      => $paypal_email,
 			'item_name'     => it_exchange_get_cart_description(),
 
-			'return='       => add_query_arg( 'it-exchange-transaction-method', 'paypal-standard', it_exchange_get_page_url( 'transaction' ) ),
+			'return'       => add_query_arg( 'it-exchange-transaction-method', 'paypal-standard', it_exchange_get_page_url( 'transaction' ) ),
 			'currency_code' => $general_settings['default-currency'],
 			'notify_url'    => get_site_url() . '/?' . it_exchange_get_webhook( 'paypal-standard' ) . '=1',
 			'no_note'       => '1',
