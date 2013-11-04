45b39f3adfa742f7776ab595dcb74566ca740103
Glenn Ansley <glenn@ithemes.com>
2013-11-04 12:20:45 -0600

    Remove Guest Log in form on Chekcout Page via JS when Log in or Registration link is clicked

--- core-addons/admin/guest-checkout/lib/assets/js/checkout.js
+++ core-addons/admin/guest-checkout/lib/assets/js/checkout.js
@@ -8,4 +8,11 @@ jQuery( function() {
 		jQuery('.it-exchange-content-checkout-logged-in-purchase-requirement-guest-checkout-link').addClass('it-exchange-hidden');
 		jQuery('.checkout-purchase-requirement-guest-checkout').removeClass('it-exchange-hidden');
 	});
+
+	jQuery(document).on('click', 'a.it-exchange-button', function(event) {
+		event.preventDefault();
+		console.log(this);
+		if ( ! jQuery(this).hasClass( 'it-exchange-login-requirement-guest-checkout') )
+			jQuery('.checkout-purchase-requirement-guest-checkout').addClass('it-exchange-hidden');
+	});
 });
