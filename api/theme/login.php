d405975296745bb9ac5867437f63a37aae0cf3d1
Glenn Ansley <glenn@ithemes.com>
2013-11-04 13:21:12 -0600

    Add breaks to switch statements in api/theme/login.php

--- api/theme/login.php
+++ api/theme/login.php
@@ -6,14 +6,14 @@
 */
 
 class IT_Theme_API_Login implements IT_Theme_API {
-	
+
 	/**
 	 * API context
 	 * @var string $_context
 	 * @since 0.4.0
 	*/
 	private $_context = 'login';
-	
+
 	/**
 	 * Current customer being viewed
 	 * @var string $_customer
@@ -88,10 +88,10 @@ class IT_Theme_API_Login implements IT_Theme_API {
 			$class= empty( $options['class'] ) ? 'it-exchange-sw-log-in' : 'it-exchange-sw-log-in ' . esc_attr( $class );
 		else
 			$class= empty( $options['class'] ) ? 'it-exchange-log-in' : 'it-exchange-log-in ' . esc_attr( $class );
-		
+
 		return '<form id="loginform" class="' . $class . '" action="' . wp_login_url( $options['redirect'] ) . '" method="post">';
 	}
-	
+
 	/**
 	 * Outputs the login's username data
 	 *
@@ -104,31 +104,31 @@ class IT_Theme_API_Login implements IT_Theme_API {
 			'label'  => __( 'Username', 'LION' ),
 		);
 		$options = ITUtility::merge_defaults( $options, $defaults );
-		
+
 		$field_id = 'user_login';
 		$field_name = 'log';
-		
+
 		switch( $options['format'] ) {
-			
+
 			case 'field-id':
 				$output = $field_id;
-			
+				break;
 			case 'field-name':
 				$output = $field_name;
-			
+				break;
 			case 'label':
 				$output = $options['label'];
-			
+				break;
 			case 'html':
 			default:
 				$output = '<label for="' . $field_id. '">' . $options['label'] . '</label>';
 				$output .= '<input type="text" id="' . $field_id. '" name="' . $field_name. '" value="" />';
-			
+
 		}
-		
+
 		return $output;
 	}
-	
+
 	/**
 	 * Outputs the login's password input data
 	 *
@@ -141,31 +141,31 @@ class IT_Theme_API_Login implements IT_Theme_API {
 			'label'  => __( 'Password', 'LION' ),
 		);
 		$options = ITUtility::merge_defaults( $options, $defaults );
-		
+
 		$field_id = 'user_pass';
 		$field_name = 'pwd';
-		
+
 		switch( $options['format'] ) {
-			
+
 			case 'field-id':
 				$output = $field_id;
-			
+				break;
 			case 'field-name':
 				$output = $field_name;
-			
+				break;
 			case 'label':
 				$output = $options['label'];
-			
+				break;
 			case 'html':
 			default:
 				$output = '<label for="' . $field_id. '">' . $options['label'] . '</label>';
 				$output .= '<input type="password" id="' . $field_id. '" name="' . $field_name. '" value="" />';
-			
+
 		}
-		
+
 		return $output;
 	}
-	
+
 	/**
 	 * Outputs the login's remember me input data
 	 *
@@ -178,31 +178,31 @@ class IT_Theme_API_Login implements IT_Theme_API {
 			'label'  => __( 'Remember Me', 'LION' ),
 		);
 		$options = ITUtility::merge_defaults( $options, $defaults );
-		
+
 		$field_id = 'rememberme';
 		$field_name = $field_id;
-		
+
 		switch( $options['format'] ) {
-			
+
 			case 'field-id':
 				$output = $field_id;
-			
+				break;
 			case 'field-name':
 				$output = $field_name;
-			
+				break;
 			case 'label':
 				$output = $options['label'];
-			
+				break;
 			case 'html':
 			default:
 				$output = '<input type="checkbox" id="' . $field_id. '" name="' . $field_name. '" value="forever" />';
 				$output .= '<label for="' . $field_id. '">' . $options['label'] . '</label>';
-			
+
 		}
-		
+
 		return $output;
 	}
-	
+
 	/**
 	 * Outputs the login page login button
 	 *
@@ -215,30 +215,30 @@ class IT_Theme_API_Login implements IT_Theme_API {
 			'label'  => __( 'Log In', 'LION' ),
 		);
 		$options = ITUtility::merge_defaults( $options, $defaults );
-		
+
 		$field_id = 'wp-submit';
 		$field_name = $field_id;
-		
+
 		switch( $options['format'] ) {
-			
+
 			case 'field-id':
 				$output = $field_id;
-			
+				break;
 			case 'field-name':
 				$output = $field_name;
-			
+				break;
 			case 'label':
 				$output = $options['label'];
-			
+				break;
 			case 'html':
 			default:
 				$output = '<input type="submit" id="' . $field_id. '" name="' . $field_name. '" value="' . $options['label'] . '" />';
-			
+
 		}
-		
+
 		return $output;
 	}
-	
+
 	/**
 	 * Outputs the login page login button
 	 *
@@ -252,31 +252,31 @@ class IT_Theme_API_Login implements IT_Theme_API {
 			'class'  => false,
 		);
 		$options = ITUtility::merge_defaults( $options, $defaults );
-		
+
 		$field_id = 'wp-submit';
 		$field_name = $field_id;
 		$class = empty( $options['class'] ) ? 'it-exchange-sw-lost-pass-link' : 'it-exchange-sw-lost-pass-link ' . $options['class'];
-		
+
 		switch( $options['format'] ) {
-			
+
 			case 'text':
 				$output = $options['label_recover'];
-				
+				break;
 			case 'url':
 				$output = esc_attr( wp_lostpassword_url() );
-			
+				break;
 			case 'label':
 				$output = $options['label'];
-			
+				break;
 			case 'html':
 			default:
 				$output = '<a class="' . esc_attr( $class ) . '" href="' . esc_attr( wp_lostpassword_url() ) . '">' . esc_attr( $options['label'] ) . '</a>';
-			
+
 		}
-		
+
 		return $output;
 	}
-	
+
 	/**
 	 * Outputs the registration link by default. 
 	 *
@@ -292,28 +292,28 @@ class IT_Theme_API_Login implements IT_Theme_API {
 			'class'  => false,
 		);
 		$options = ITUtility::merge_defaults( $options, $defaults );
-		
+
 		$field_id = 'wp-submit';
 		$field_name = $field_id;
 		$class = empty( $options['class'] ) ? 'it-exchange-sw-register-link' : 'it-exchange-sw-register-link ' . $options['class'];
-		
+
 		switch( $options['format'] ) {
-				
+
 			case 'url':
 				$output = it_exchange_get_page_url( 'registration' );
-			
+				break;
 			case 'label':
 				$output = esc_attr( $options['label'] );
-			
+				break;
 			case 'html':
 			default:
 				$output = '<a class="' . esc_attr( $class ) . '" href="' . it_exchange_get_page_url( 'registration' ) . '">' . esc_attr( $options['label'] ) . '</a>';
-			
+
 		}
-		
+
 		return $output;
 	}
-	
+
 	/**
 	 * Outputs the login page cancel button
 	 *
@@ -327,32 +327,32 @@ class IT_Theme_API_Login implements IT_Theme_API {
 			'class'  => false,
 		);
 		$options = ITUtility::merge_defaults( $options, $defaults );
-		
+
 		$field_id = 'it-exchange-cancel-login-customer';
 		$field_name = $field_id;
 		$class = empty( $options['class'] ) ? 'it-exchange-sw-cancel-login-link' : 'it-exchange-sw-cancel-login-link ' . $options['class'];
-		
+
 		if ( it_exchange_is_multi_item_cart_allowed() )
 			$page = 'cart';
 		else
 			$page = 'product';
-		
+
 		switch( $options['format'] ) {
-				
+
 			case 'url':
 				$output = it_exchange_get_page_url( $page );
-			
+
 			case 'label':
 				$output = esc_attr( $options['label'] );
-			
+
 			case 'html':
 			default:
 				$output = '<a class="' . esc_attr( $class ) . '" href="' . it_exchange_get_page_url( $page ) . '">' .esc_attr( $options['label'] ) . '</a>';
-			
+
 		}
 		return $output;
 	}
-	
+
 	/**
 	 * Outputs the closing form tag for the login form.
 	 *
