480ccc2c9b5541f35ff24496651ccec8ce58c3ee
Justin Kopepasah <justin@kopepasah.com>
2013-11-05 10:33:00 -0600

    Initial styles added for guest checkout in super widget.

--- core-addons/admin/guest-checkout/lib/templates/super-widget-guest-checkout.php
+++ core-addons/admin/guest-checkout/lib/templates/super-widget-guest-checkout.php
@@ -14,14 +14,16 @@
 */
 ?>
 <?php do_action( 'it_exchange_super_widget_guest_checkout_before_wrap' ); ?>
-<div class="guest-checkout it-exchange-sw-processing-guest-checkout">
+<div class="guest-checkout it-exchange-sw-processing-guest-checkout it-exchange-sw-processing">
 	<?php do_action( 'it_exchange_super_widget_guest_checkout_begin_wrap' ); ?>
 	<?php it_exchange_get_template_part( 'messages' ); ?>
 	<?php echo it_exchange_guest_checkout_get_heading(); ?>
-	<form class="it-exchange-guest-checkout-form" method="post" action="">
-		<?php it_exchange_get_template_part( 'super-widget-guest-checkout/loops/fields' ); ?>
-		<?php it_exchange_get_template_part( 'super-widget-guest-checkout/loops/actions' ); ?>
-	</form>
+	<div class="it-exchange-guest-checkout-form-wrapper it-exchange-clearfix">
+		<form class="it-exchange-guest-checkout-form" method="post" action="">
+			<?php it_exchange_get_template_part( 'super-widget-guest-checkout/loops/fields' ); ?>
+			<?php it_exchange_get_template_part( 'super-widget-guest-checkout/loops/actions' ); ?>
+		</form>
+	</div>
 	<?php do_action( 'it_exchange_super_widget_guest_checkout_end_wrap' ); ?>
 </div>
 <?php do_action( 'it_exchange_super_widget_guest_checkout_after_wrap' ); ?>
