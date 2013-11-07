38c4414487488e6edc204a8f0e98fd476f70d6da
Glenn Ansley <glenn@ithemes.com>
2013-11-07 09:22:25 -0600

    Make default logout redirect go to login page if it isn't disabled. Go to site home if it is. Filter i just added still works though.\n #blameCory

--- lib/pages/class.pages.php
+++ lib/pages/class.pages.php
@@ -191,7 +191,8 @@ class IT_Exchange_Pages {
 			wp_redirect( it_exchange_get_page_url( 'profile' ) );
 			die();
 		} else if ( is_user_logged_in() && 'logout' == $this->_current_view ) {
-			$url = apply_filters( 'it_exchange_redirect_on_logout', str_replace( '&amp;', '&', wp_logout_url( it_exchange_get_page_url( 'store' ) ) ) );
+			$default = 'disabled' == it_exchange_get_page_type( 'login' ) ? get_home_url() : str_replace( '&amp;', '&', wp_logout_url( it_exchange_get_page_url( 'login', false, true ) ) );
+			$url = apply_filters( 'it_exchange_redirect_on_logout', $default );
 			wp_redirect( $url );
 			die();
 		} else if ( ! is_user_logged_in() && 'logout' == $this->_current_view ) {
