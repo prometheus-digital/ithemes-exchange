8831b9dffed5ec4c474748b60f389644830213d1
Glenn Ansley <glenn@ithemes.com>
2013-11-25 11:32:09 -0600

    Massage the history.txt and the init.php after the bot did its thing.

--- init.php
+++ init.php
@@ -23,7 +23,7 @@
 */
 class IT_Exchange {
 
-	var $_version         = '1.6.2';
+	var $_version         = '1.6.3';
 	var $_wp_minimum      = '3.5';
 	var $_slug            = 'it-exchange';
 	var $_name            = 'iThemes Exchange';
