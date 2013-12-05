380558e5e852a3a0eb3079cb23f9ade3a6cd439e
Lew Ayotte <lew@ithemes.com>
2013-12-05 08:21:31 -0600

    changed user profile exchange label

--- lib/admin/views/admin-user-profile.php
+++ lib/admin/views/admin-user-profile.php
@@ -19,7 +19,7 @@ $user_object = get_userdata( $user_id );
 <table class="form-table">
 <tbody>
 <tr id="it_exchange_customer_data">
-    <th><?php _e( 'This user is an Exchange customer.', 'LION' ); ?></th>
+    <th><?php _e( 'iThemes Exchange', 'LION' ); ?></th>
     <td>
     <?php echo "<a class='it-exchange-cust-info' href='" . esc_url( add_query_arg( array( 'wp_http_referer' => urlencode( stripslashes_deep( $_SERVER['REQUEST_URI'] ) ), 'it_exchange_customer_data' => 1 ), get_edit_user_link( $user_object->ID ) ) ) . "'>" . __( 'View Customer Data', 'LION' ) . "</a>"; ?>
     </td>
