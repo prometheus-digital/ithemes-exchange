e4e5508cdcc85e0fe8ecf2df0c7465fb534bbdd4
Lew Ayotte <lew@ithemes.com>
2014-03-19 07:36:11 -0500

    Tagging 1.7.19

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange
- * Version: 1.7.18
+ * Version: 1.7.19
  * Description: Easily sell your digital goods with iThemes Exchange, simple ecommerce for WordPress
  * Plugin URI: http://ithemes.com/exchange/
  * Author: iThemes
