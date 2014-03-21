7f4a892d4dd9c7ce86b37d073748ba82ccf47303
Lew Ayotte <lew@ithemes.com>
2014-03-21 10:17:02 -0500

    Forgot to change version number in ITE class

--- init.php
+++ init.php
@@ -23,7 +23,7 @@
 */
 class IT_Exchange {
 
-	var $_version         = '1.7.18';
+	var $_version         = '1.7.20';
 	var $_wp_minimum      = '3.5';
 	var $_slug            = 'it-exchange';
 	var $_name            = 'iThemes Exchange';
