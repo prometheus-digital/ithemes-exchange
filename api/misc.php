a70495ba5af46693d3ddd388ebc0a6fcdabeb935
Lew Ayotte <lew@ithemes.com>
2014-05-21 15:06:28 -0500

    Setup menu items filter to compare path of login/logout URLS, not the full URLs, in case of https/http conflict

--- api/misc.php
+++ api/misc.php
@@ -211,8 +211,11 @@ function it_exchange_clean_query_args( $exempt=array(), $additional=array() ) {
 function it_exchange_wp_get_nav_menu_items_filter( $items, $menu, $args ) {
 	if ( is_user_logged_in() ) {
 		foreach ( $items as $item ) {
-			if ( $item->url == it_exchange_get_page_url( 'login' ) || $item->url == it_exchange_get_page_url( 'logout' ) ) {
-
+			//We really just want to compare the URL PATH, so grab that and compare later
+			$item_url_path = parse_url( $item->url, PHP_URL_PATH );
+			$login_url_path = parse_url( it_exchange_get_page_url( 'login' ), PHP_URL_PATH );
+			$logout_url_path = parse_url( it_exchange_get_page_url( 'logout' ), PHP_URL_PATH );
+			if ( $item_url_path == $login_url_path || $item_url_path == $logout_url_path ) {
 				$item->url = it_exchange_get_page_url( 'logout' );
 				$item->title = it_exchange_get_page_name( 'logout' );
 			}
