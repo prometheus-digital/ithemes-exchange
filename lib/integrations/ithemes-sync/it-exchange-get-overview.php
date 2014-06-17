2628fb7ded78d5413d5664d71153422aa3d9aca6
Aaron D. Campbell <aaron@xavisys.com>
2014-06-17 11:53:17 -0500

    CamelCase for Sync

--- lib/integrations/ithemes-sync/it-exchange-get-overview.php
+++ lib/integrations/ithemes-sync/it-exchange-get-overview.php
@@ -12,24 +12,24 @@ class Ithemes_Sync_Verb_Ithemes_Exchange_Get_Overview extends Ithemes_Sync_Verb 
 	 *     array(
 	 *         'version'  => {current-exchange-version}
 	 *         'overview' => array(
-	 *             'sales-today'             => {sales-today-as-currency}
-	 *             'sales-this-month'        => {sales-this-month-as-currency}
-	 *             'transactions-today'      => {number-of-transactions-today-as-int}
-	 *             'transactions-this-month' => {number-of-transactions-this-month-as-int}
-	 *             'recent-transactions'     => {array-of-5-most-recent-transactions}
-	 *             'all-transactions-url'    => {url-to-wp-admin-transactions-list}
+	 *             'salesToday'            => {sales-today-as-currency}
+	 *             'salesThisMonth'        => {sales-this-month-as-currency}
+	 *             'transactionsToday'     => {number-of-transactions-today-as-int}
+	 *             'transactionsThisMonth' => {number-of-transactions-this-month-as-int}
+	 *             'recentTransactions'    => {array-of-5-most-recent-transactions}
+	 *             'allTransactionsUrl'    => {url-to-wp-admin-transactions-list}
 	 *         )
 	 *     )
 	 */
 	public function run( $args ) {
 
 		$overview = array(
-			'sales-today'             => it_exchange_basic_reporting_get_total( array( 'start_time' => strtotime( 'today' ), 'end_time' => ( strtotime( 'tomorrow' ) - 1 ) ) ),
-			'sales-this-month'        => it_exchange_basic_reporting_get_total( array( 'start_time' => strtotime( date( 'Y-m-01' ) ) ) ),
-			'transactions-today'      => it_exchange_basic_reporting_get_transactions_count( array( 'start_time' => strtotime( 'today' ), 'end_time' => ( strtotime( 'tomorrow' ) - 1 ) ) ),
-			'transactions-this-month' => it_exchange_basic_reporting_get_transactions_count( array( 'start_time' => strtotime( date( 'Y-m-01' ) ) ) ),
-			'recent-transactions'     => array(),
-			'all-transactions-url'    => get_admin_url( null, 'edit.php?post_type=it_exchange_tran' ),
+			'salesToday'            => it_exchange_basic_reporting_get_total( array( 'start_time' => strtotime( 'today' ), 'end_time' => ( strtotime( 'tomorrow' ) - 1 ) ) ),
+			'salesThisMonth'        => it_exchange_basic_reporting_get_total( array( 'start_time' => strtotime( date( 'Y-m-01' ) ) ) ),
+			'transactionsToday'     => it_exchange_basic_reporting_get_transactions_count( array( 'start_time' => strtotime( 'today' ), 'end_time' => ( strtotime( 'tomorrow' ) - 1 ) ) ),
+			'transactionsThisMonth' => it_exchange_basic_reporting_get_transactions_count( array( 'start_time' => strtotime( date( 'Y-m-01' ) ) ) ),
+			'recentTransactions'    => array(),
+			'allTransactionsUrl'    => get_admin_url( null, 'edit.php?post_type=it_exchange_tran' ),
 		);
 
 		if ( $transactions = it_exchange_get_transactions( array( 'posts_per_page' => 5 ) ) ) {
@@ -42,13 +42,13 @@ class Ithemes_Sync_Verb_Ithemes_Exchange_Get_Overview extends Ithemes_Sync_Verb 
 				register_post_type( 'it_exchange_tran' );
 			}
 			foreach( $transactions as $transaction ) {
-				$overview['recent-transactions'][] = array(
-					'date'                 => it_exchange_get_transaction_date( $transaction ),
-					'order-number'         => it_exchange_get_transaction_order_number( $transaction ),
-					'total'                => it_exchange_get_transaction_total( $transaction ),
-					'cleared-for-delivery' => it_exchange_transaction_is_cleared_for_delivery( $transaction ),
-					'status-label'         => it_exchange_get_transaction_status_label( $transaction ),
-					'edit-url'             => get_edit_post_link( $transaction->ID, 'raw' ),
+				$overview['recentTransactions'][] = array(
+					'date'               => it_exchange_get_transaction_date( $transaction ),
+					'orderNumber'        => it_exchange_get_transaction_order_number( $transaction ),
+					'total'              => it_exchange_get_transaction_total( $transaction ),
+					'clearedForDelivery' => it_exchange_transaction_is_cleared_for_delivery( $transaction ),
+					'statusLabel'        => it_exchange_get_transaction_status_label( $transaction ),
+					'editUrl'            => get_edit_post_link( $transaction->ID, 'raw' ),
 				);
 			}
 		}
