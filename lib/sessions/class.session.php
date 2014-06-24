3877f419cff4a659ec6e072db43f12076a89826f
Lew Ayotte <lew@ithemes.com>
2014-06-24 13:36:25 -0500

    Empty arrays passed through json_encode result in a string that causes json_decode to output an array, but an object is expected by get_object_vars, which outputs a warning on some PHP installs. Checking for empty arrays in session for Cart Sync.

--- lib/sessions/class.session.php
+++ lib/sessions/class.session.php
@@ -80,9 +80,11 @@ class IT_Exchange_Session {
 			if ( $key && !empty( $this->_session[$key] ) )
 				return $this->_session[$key];
 		} else {
-			if ( $session_data = get_object_vars( json_decode( it_exchange_db_session_encode() ) ) ) {
-				$session_data = array_map( 'maybe_unserialize', $session_data );
-				return $session_data;
+			if ( $json = it_exchange_db_session_encode() && !empty( $json ) ) {
+				if ( $session_data = get_object_vars( json_decode( $json ) ) ) {
+					$session_data = array_map( 'maybe_unserialize', $session_data );
+					return $session_data;
+				}
 			}
 		}
 		return array();
