fd1f052ae4d927fb26c9f328148894a234f397c3
Git User <git@petra.ithemes.com>
2014-09-11 14:50:22 -0500

    1.10.5: updated: lib/icon-fonts

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange
- * Version: 1.10.4
+ * Version: 1.10.5
  * Text Domain: LION
  * Description: Easily sell your digital goods with iThemes Exchange, simple ecommerce for WordPress
  * Plugin URI: http://ithemes.com/exchange/
