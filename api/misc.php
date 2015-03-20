6fa0f6f2dbeb47d7c2ac887ab5b4416f745227fe
Lew Ayotte <lew@ithemes.com>
2015-03-20 09:33:23 -0500

    Fix for converting to DB number for numbers with unexpected number of decimals places (I said 'number' three times in this commit message, four now)

--- api/misc.php
+++ api/misc.php
@@ -101,10 +101,15 @@ function it_exchange_convert_to_database_number( $price ) {
 
 	$price = html_entity_decode( trim( $price ), ENT_COMPAT, 'UTF-8' );
 
-	if ( strstr( $price, $sep ) )
+	if ( strstr( $price, $sep ) ) {
+		if ( '.' !== $sep ) {
+			$price = str_replace( $sep, '.', $price );
+		}
+		$price = number_format( (float)$price, 2 ); //make sure we have 2 decimal places!
 		$price = preg_replace("/[^0-9]*/", '', $price );
-	else //if we don't find a decimal separator, we want to multiply by 100 for future decimal operations
+	} else { //if we don't find a decimal separator, we want to multiply by 100 for future decimal operations
 		$price = preg_replace("/[^0-9]*/", '', $price ) * 100;
+	}
 	
 	return $price;
 }
