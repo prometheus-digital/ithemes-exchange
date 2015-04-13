91e368eb047c2ca5a4d4af13421e686d8f46564b
Lew Ayotte <lew@ithemes.com>
2015-04-13 08:26:27 -0500

    Tagging 1.11.17

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange
- * Version: 1.11.16
+ * Version: 1.11.17
  * Text Domain: it-l10n-ithemes-exchange
  * Description: Easily sell your digital goods with iThemes Exchange, simple ecommerce for WordPress
  * Plugin URI: http://ithemes.com/exchange/
@@ -24,7 +24,7 @@
 */
 class IT_Exchange {
 
-	var $_version         = '1.11.16';
+	var $_version         = '1.11.17';
 	var $_wp_minimum      = '3.5';
 	var $_slug            = 'ithemes-exchange';
 	var $_name            = 'iThemes Exchange';
