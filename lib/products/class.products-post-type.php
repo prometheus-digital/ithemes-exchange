bc4faeab8d2a36284e4341400fbcd8511b5eadf4
Glenn Ansley <glenn@ithemes.com>
2015-04-15 12:53:54 -0500

    Move esc_url to href attribute for preview product links

--- lib/products/class.products-post-type.php
+++ lib/products/class.products-post-type.php
@@ -241,17 +241,17 @@ class IT_Exchange_Product_Post_Type {
 					<div id="preview-action">
 						<?php
 							if ( 'publish' == $post->post_status ) {
-								$preview_link = esc_url( apply_filters( 'it_exchange_view_product_button_link', get_permalink( $post->ID ), $post ) );
+								$preview_link = apply_filters( 'it_exchange_view_product_button_link', get_permalink( $post->ID ), $post );
 								$preview_button = apply_filters( 'it_exchange_view_product_button_label', __( 'View Product', 'it-l10n-ithemes-exchange' ), $post );
 								$preview_id = 'post-view';
 							} else {
 								$preview_link = set_url_scheme( get_permalink( $post->ID ) );
-								$preview_link = esc_url( apply_filters( 'it_exchange_preview_product_button_link', apply_filters( 'preview_post_link', add_query_arg( 'preview', 'true', $preview_link ) ), $post ) );
+								$preview_link = apply_filters( 'it_exchange_preview_product_button_link', apply_filters( 'preview_post_link', add_query_arg( 'preview', 'true', $preview_link ) ), $post );
 								$preview_button = apply_filters( 'it_exchange_preview_product_button_label', __( 'Preview Product', 'it-l10n-ithemes-exchange' ), $post );
 								$preview_id = 'post-preview';
 							}
 						?>
-						<a class="preview button button-large" href="<?php echo $preview_link; ?>" target="wp-preview" id="<?php echo $preview_id; ?>"><?php echo $preview_button; ?></a>
+						<a class="preview button button-large" href="<?php echo esc_url( $preview_link ); ?>" target="wp-preview" id="<?php echo $preview_id; ?>"><?php echo $preview_button; ?></a>
 						<input type="hidden" name="wp-preview" id="wp-preview" value="" />
 					</div>
 				<?php endif; ?>
