9e9e0e82d7551ffcc8e10aa311ae330feaf2a37a
Timothy Jacobs <timothy@ironbounddesigns.com>
2015-11-24 12:46:16 -0500

    Add 'Embed Super Widget' button to create the shortcode The embed button is responsible for creating the shortcode for the user. It allows for selecting a product from a list, and toggling the display of the product short description. By default, the description is hidden.

--- lib/shortcodes/shortcodes.php
+++ lib/shortcodes/shortcodes.php
@@ -27,6 +27,8 @@ class IT_Exchange_SW_Shortcode {
 	public function __construct() {
 		add_shortcode( 'it_exchange_sw', array( $this, 'callback' ) );
 		add_action( 'it_exchange_enabled_addons_loaded', array( $this, 'register_feature' ) );
+		add_action( 'media_buttons', array( $this, 'insert_button' ) );
+		add_action( 'admin_footer', array( $this, 'thickbox' ) );
 	}
 
 	/**
@@ -50,9 +52,129 @@ class IT_Exchange_SW_Shortcode {
 	 */
 	public function register_feature() {
 
-		$desc = __( "Allows products to be embedded in a shortcode.", 'it-l10n-ihemes-exchange' );
+		$desc = __( "Allows products to be embedded in a shortcode.", 'it-l10n-ithemes-exchange' );
 
 		it_exchange_register_product_feature( 'sw-shortcode', $desc );
+	}
+
+	/**
+	 * Insert the embed a SW button.
+	 *
+	 * This is embedded only for public post types, and NOT for products.
+	 *
+	 * @since 1.33
+	 */
+	public function insert_button() {
+
+		$screen = get_current_screen();
+
+		$post_type = ! empty( $screen->post_type ) ? $screen->post_type : get_post_type();
+		$post_type = get_post_type_object( $post_type );
+
+		if ( $post_type->public && $post_type != 'it_exchange_prod' ) {
+			add_thickbox();
+			$id    = 'it-exchange-insert-sw-shortcode';
+			$class = 'thickbox button it-exchange-insert-sw-shortcode';
+			$title = __( "Embed Super Widget", 'it-l10n-ithemes-exchange' );
+			echo '<a href="#TB_inline?width=150height=250&inlineId=' . $id . '" class="' . $class . '" title="' . $title . '"> ' . $title . '</a>';
+		}
+	}
+
+	/**
+	 * Render the thickbox for inserting a SW shortcode.
+	 *
+	 * @since 1.33
+	 */
+	public function thickbox() {
+
+		$screen = get_current_screen();
+
+		$post_type = ! empty( $screen->post_type ) ? $screen->post_type : get_post_type();
+		$post_type = get_post_type_object( $post_type );
+
+		if ( ! $post_type->public || $post_type == 'it_exchange_prod' ) {
+			return;
+		}
+
+		$product_types = it_exchange_get_addons( array(
+			'category' => 'product-type'
+		) );
+
+		foreach ( $product_types as $product_type => $addon ) {
+			if ( ! it_exchange_product_type_supports_feature( $product_type, 'sw-shortcode' ) ) {
+				unset( $product_types[ $product_type ] );
+			}
+		}
+
+		$products = it_exchange_get_products( array(
+			'show_hidden' => true,
+			'meta_query'  => array(
+				array(
+					'key'     => '_it_exchange_product_type',
+					'compare' => 'IN',
+					'value'   => array_keys( $product_types )
+				)
+			)
+		) );
+		?>
+
+		<script type="text/javascript">
+			jQuery(document).ready(function ($) {
+				$(document).on('click', '#it-exchange-sw-insert', function (e) {
+					var prod = jQuery("#it-exchange-sw-product").val();
+					if (prod.length == 0 || prod == -1) {
+						alert("<?php echo esc_js( __( "You must select a product", 'it-l10n-ithemes-exchange' ) ); ?>");
+						return;
+					}
+
+					var desc = '';
+
+					if (jQuery("#it-exchange-sw-description").is(':checked')) {
+						desc = ' description="yes"';
+					}
+
+					var short = '[it_exchange_sw product="' + prod + '"' + desc + ']';
+
+					window.send_to_editor(short);
+					tb_remove();
+				});
+			});
+		</script>
+
+		<div id="it-exchange-insert-sw-shortcode" style="display: none">
+			<div class="wrap">
+
+				<div>
+					<label for="it-exchange-sw-product"><?php _e( 'Select a Product', 'it-l10n-ithemes-exchange' ); ?></label><br>
+					<select id="it-exchange-sw-product">
+						<option value="-1"><?php _e( 'Select', 'it-l10n-ithemes-exchange' ); ?></option>
+
+						<?php foreach ( $products as $product ): ?>
+							<option value="<?php echo $product->ID; ?>">
+								<?php echo $product->post_title; ?>
+							</option>
+						<?php endforeach; ?>
+					</select>
+
+					<br><br>
+
+					<input type="checkbox" id="it-exchange-sw-description">
+					<label for="it-exchange-sw-description">
+						<?php _e( "Include product description?", 'it-l10n-ithemes-exchange' ); ?>
+					</label>
+				</div>
+
+				<div style="padding: 15px 15px 15px 0">
+					<input type="button" class="button-primary" id="it-exchange-sw-insert" value="<?php _e( "Embed", 'it-l10n-ithemes-exchange' ); ?>" />
+					&nbsp;&nbsp;&nbsp;
+					<a class="button" style="color:#bbb;" href="#" onclick="tb_remove(); return false;">
+						<?php _e( "Cancel", 'it-l10n-ithemes-exchange' ); ?>
+					</a>
+				</div>
+			</div>
+		</div>
+
+		<?php
 	}
 
 	/**
@@ -67,7 +189,7 @@ class IT_Exchange_SW_Shortcode {
 	 */
 	public function callback( $atts ) {
 
-		$atts = shortcode_atts( array( 'product' => null, 'description' => false ), $atts, 'it_exchange_sw' );
+		$atts = shortcode_atts( array( 'product' => null, 'description' => 'no' ), $atts, 'it_exchange_sw' );
 
 		$product      = it_exchange_get_product( $atts['product'] );
 		$product_type = it_exchange_get_product_type( $product->ID );
@@ -91,7 +213,7 @@ class IT_Exchange_SW_Shortcode {
 		it_exchange_set_product( $product->ID );
 		$this->product = $product;
 
-		if ( ! $atts['description'] ) {
+		if ( $atts['description'] !== 'yes' ) {
 			$this->hide_parts[] = 'description';
 		}
 
