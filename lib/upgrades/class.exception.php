a79e32d6a2c1270a5cbcdc868901a04e8a755ea6
Timothy Jacobs <timothy@ironbounddesigns.com>
2015-12-13 15:53:48 -0500

    First pass at upgrade framework WIP
    Introduces four new classes and interfaces.
    
    - `IT_Exchange_Upgrade_Config`
    - `IT_Exchange_Upgrade_Exception`
    - `IT_Exchange_Upgrade_SkinInterface`
    - `IT_Exchange_UpgradeInterface`
    
    This is still very much a work in progress. The interfaces are defined
    for the actual upgrade routines and feedback mechanisms.

---
+++ lib/upgrades/class.exception.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Upgrade routine exception.
+ *
+ * @since   1.33
+ * @license GPLv2
+ */
+
+class IT_Exchange_Upgrade_Exception extends Exception {}
---
+++ lib/upgrades/interface.upgrade.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Upgrade routine interface.
+ *
+ * @since   1.33
+ * @license GPLv2
+ */
+
+/**
+ * Interface IT_Exchange_UpgradeInterface
+ */
+interface IT_Exchange_UpgradeInterface {
+
+	/**
+	 * Get the iThemes Exchange version this upgrade applies to.
+	 *
+	 * @since 1.33
+	 *
+	 * @return string
+	 */
+	public function get_version();
+
+	/**
+	 * Get the name of this upgrade.
+	 *
+	 * @since 1.33
+	 *
+	 * @return string
+	 */
+	public function get_name();
+
+	/**
+	 * Get the slug for this upgrade. This should be globally unique.
+	 *
+	 * @since 1.33
+	 *
+	 * @return string
+	 */
+	public function get_slug();
+
+	/**
+	 * Get the description for this upgrade. 1-3 sentences.
+	 *
+	 * @since 1.33
+	 *
+	 * @return string
+	 */
+	public function get_description();
+
+	/**
+	 * Get the total records needed to be processed for this upgrade.
+	 *
+	 * This is used to build the upgrade UI.
+	 *
+	 * @since 1.33
+	 *
+	 * @return int
+	 */
+	public function get_total_records_to_process();
+
+	/**
+	 * Perform the upgrade according to the given configuration.
+	 *
+	 * Throwing an upgrade exception will halt the upgrade process and notify the user.
+	 *
+	 * @param IT_Exchange_Upgrade_Config        $config
+	 * @param IT_Exchange_Upgrade_SkinInterface $skin
+	 *
+	 * @return void
+	 *
+	 * @throws IT_Exchange_Upgrade_Exception
+	 */
+	public function upgrade( IT_Exchange_Upgrade_Config $config, IT_Exchange_Upgrade_SkinInterface $skin );
+}
