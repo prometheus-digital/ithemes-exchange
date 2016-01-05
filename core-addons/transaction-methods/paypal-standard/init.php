e75942e91ca90841a7c9e601354403e704179fcb
Timothy Jacobs <timothy@ironbounddesigns.com>
2016-01-05 16:12:45 -0500

    Send a 400 status header if IPN verification fails

--- core-addons/transaction-methods/paypal-standard/init.php
+++ core-addons/transaction-methods/paypal-standard/init.php
@@ -798,6 +798,9 @@ function it_exchange_paypal_standard_addon_process_webhook( $request ) {
 	$body           = wp_remote_retrieve_body( $response );
 
 	if ( 'VERIFIED' !== $body ) {
+
+		status_header( 400 );
+
 		error_log( sprintf( __( 'Invalid IPN sent from PayPal - PayLoad: %s', 'it-l10n-ithemes-exchange' ), maybe_serialize( $payload ) ) );
 		error_log( sprintf( __( 'Invalid IPN sent from PayPal - Response: %s', 'it-l10n-ithemes-exchange' ), maybe_serialize( $response ) ) );
 
