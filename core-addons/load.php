9e5f8730bc6a9aca5c28a0d8c909e32ead683ae9
Timothy Jacobs <timothy@ironbounddesigns.com>
2016-01-07 16:03:55 -0500

    Update description, add purchase link if ITSEC is not active

--- core-addons/load.php
+++ core-addons/load.php
@@ -264,7 +264,7 @@ function it_exchange_register_core_addons() {
 		),
 		'ithemes-security'      => array(
 			'name'              => __( 'iThemes Security', 'it-l10n-ithemes-exchange' ),
-			'description'       => __( 'Use iThemes Security to power recaptcha for login and registration forms.', 'it-l10n-ithemes-exchange' ),
+			'description'       => __( 'Secure your Exchange login and registration forms with iThemes Security.', 'it-l10n-ithemes-exchange' ),
 			'author'            => 'iThemes',
 			'author_url'        => 'http://ithemes.com',
 			'file'              => dirname( __FILE__ ) . '/admin/ithemes-security/init.php',
