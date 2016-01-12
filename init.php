13846faecab92bb7ab70bd506b7d229d0150e1d3
Timothy Jacobs <timothy@ironbounddesigns.com>
2016-01-12 14:52:22 -0500

    Correct version number in plugin header

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange
- * Version: 1.32.2
+ * Version: 1.33
  * Text Domain: it-l10n-ithemes-exchange
  * Description: Easily sell your digital goods with iThemes Exchange, simple ecommerce for WordPress
  * Plugin URI: http://ithemes.com/exchange/
