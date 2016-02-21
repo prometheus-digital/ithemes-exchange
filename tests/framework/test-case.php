a54bae797f2c245dd3a09024e359dba80346ffbd
Timothy Jacobs <timothy@ironbounddesigns.com>
2016-02-20 21:35:57 -0500

    Make test factories static

--- tests/framework/test-case.php
+++ tests/framework/test-case.php
@@ -8,6 +8,11 @@
 
 /**
  * Class IT_Exchange_UnitTestCase
+ *
+ * @property IT_Exchange_Test_Factory_For_Products      $product_factory
+ * @property IT_Exchange_Test_Factory_For_Downloads     $download_factory
+ * @property IT_Exchange_Test_Factory_For_Transactions  $transaction_factory
+ * @property IT_Exchange_Test_Factory_For_Basic_Coupons $coupon_factory
  */
 class IT_Exchange_UnitTestCase extends WP_UnitTestCase {
 
@@ -17,43 +22,94 @@ class IT_Exchange_UnitTestCase extends WP_UnitTestCase {
 	protected $exchange_admin;
 
 	/**
-	 * @var IT_Exchange_Test_Factory_For_Products
-	 */
-	protected $product_factory;
-
-	/**
-	 * @var IT_Exchange_Test_Factory_For_Downloads
-	 */
-	protected $download_factory;
-
-	/**
-	 * @var IT_Exchange_Test_Factory_For_Transactions
-	 */
-	protected $transaction_factory;
-
-	/**
-	 * @var IT_Exchange_Test_Factory_For_Basic_Coupons
-	 */
-	protected $coupon_factory;
-
-	/**
 	 * Do custom initialization.
 	 */
 	public function setUp() {
 		parent::setUp();
 
-		$null                      = null;
-		$this->exchange_admin      = new IT_Exchange_Admin( $null );
-		$this->product_factory     = new IT_Exchange_Test_Factory_For_Products( self::factory() );
-		$this->download_factory    = new IT_Exchange_Test_Factory_For_Downloads( self::factory() );
-		$this->transaction_factory = new IT_Exchange_Test_Factory_For_Transactions( self::factory() );
-		$this->coupon_factory      = new IT_Exchange_Test_Factory_For_Basic_Coupons( self::factory() );
+		$null                 = null;
+		$this->exchange_admin = new IT_Exchange_Admin( $null );
 
 		it_exchange_save_option( 'settings_general',
 			$this->exchange_admin->set_general_settings_defaults( array() ) );
 		it_exchange_get_option( 'settings_general', true );
 
 		WP_Mock::setUp();
+	}
+
+	/**
+	 * @return IT_Exchange_Test_Factory_For_Products|null
+	 */
+	static function product_factory() {
+		static $factory = null;
+
+		if ( ! $factory ) {
+			$factory = new IT_Exchange_Test_Factory_For_Products( self::factory() );
+		}
+
+		return $factory;
+	}
+
+	/**
+	 * @return IT_Exchange_Test_Factory_For_Downloads|null
+	 */
+	static function download_factory() {
+		static $factory = null;
+
+		if ( ! $factory ) {
+			$factory = new IT_Exchange_Test_Factory_For_Downloads( self::factory() );
+		}
+
+		return $factory;
+	}
+
+	/**
+	 * @return IT_Exchange_Test_Factory_For_Transactions|null
+	 */
+	static function transaction_factory() {
+		static $factory = null;
+
+		if ( ! $factory ) {
+			$factory = new IT_Exchange_Test_Factory_For_Transactions( self::factory() );
+		}
+
+		return $factory;
+	}
+
+	/**
+	 * @return IT_Exchange_Test_Factory_For_Basic_Coupons|null
+	 */
+	static function coupon_factory() {
+		static $factory = null;
+
+		if ( ! $factory ) {
+			$factory = new IT_Exchange_Test_Factory_For_Basic_Coupons( self::factory() );
+		}
+
+		return $factory;
+	}
+
+	/**
+	 * is utilized for reading data from inaccessible members.
+	 *
+	 * @param $name string
+	 *
+	 * @return mixed
+	 * @link http://php.net/manual/en/language.oop5.overloading.php#language.oop5.overloading.members
+	 */
+	function __get( $name ) {
+		switch ( $name ) {
+			case 'product_factory':
+				return self::product_factory();
+			case 'download_factory':
+				return self::download_factory();
+			case 'transaction_factory':
+				return self::transaction_factory();
+			case 'coupon_factory':
+				return self::coupon_factory();
+		}
+
+		return null;
 	}
 
 	/**
