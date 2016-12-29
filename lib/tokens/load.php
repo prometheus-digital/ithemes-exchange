bdfd4b2c46b072717587b08fa51fe0aa3a037a3e
Timothy Jacobs <timothy@ironbounddesigns.com>
2016-12-30 00:46:57 +0100

    Standardize on == false for checking embedded resources

--- lib/tokens/load.php
+++ lib/tokens/load.php
@@ -19,7 +19,7 @@ Manager::register( new ITE_Payment_Tokens_Table(), '', 'ITE_Payment_Token' );
 Manager::register( new BaseMetaTable( Manager::get( 'ite-payment-tokens' ), array( 'primary_id_column' => 'token' ) ) );
 
 ITE_Payment_Token::register_token_type( 'card', 'ITE_Payment_Token_Card', __( 'Card', 'it-l10n-ithemes-exchange' ) );
-ITE_Payment_Token::register_token_type( 'bank', 'ITE_Payment_Token_Bank_Account', 'Bank Account', 'it-10n-ithemes-exchange' );
+ITE_Payment_Token::register_token_type( 'bank', 'ITE_Payment_Token_Bank_Account', __( 'Bank Account', 'it-10n-ithemes-exchange' ) );
 
 add_action( 'it_exchange_register_object_types', function ( ITE_Object_Type_Registry $registry ) {
 	$registry->register( new ITE_Payment_Token_Object_Type() );
