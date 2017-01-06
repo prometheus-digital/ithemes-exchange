prometheus-digital/ithemes-exchange

commit ef0b5116799ae156492652de14f98ae3f21736ee

Camel case for sendReceipt() JS lib

lib/assets/js/rest.js
@@ -576,10 +576,10 @@
 		 *
 		 * @returns {XMLHttpRequest}
 		 */
-		send_receipt: function ( email ) {
+		sendReceipt: function ( email ) {
 			return $.ajax( {
 				method    : 'POST',
-				url       : _.result( this, 'url' ) + '/send_receipt',
+				url       : _.result( this, 'url' ) + '/sendReceipt',
 				data      : {
 					email: email,
 				},

lib/admin/js/transaction-details.js
@@ -61,7 +61,7 @@
 
 			$this.attr( 'disabled', true );
 
-			transaction.send_receipt( $email.val() ).always( function () {
+			transaction.sendReceipt( $email.val() ).always( function () {
 				$this.removeAttr( 'disabled' );
 			} ).done( function () {
 				$manager.slideUp();

commit 75a820b50caef4425beb61ca751bac6082cc5e28

Fix send receipt method in rest.js. ef0b511 accidentally change the route name

lib/assets/js/rest.js
@@ -579,7 +579,7 @@
 		sendReceipt: function ( email ) {
 			return $.ajax( {
 				method    : 'POST',
-				url       : _.result( this, 'url' ) + '/sendReceipt',
+				url       : _.result( this, 'url' ) + '/send_receipt',
 				data      : {
 					email: email,
 				},