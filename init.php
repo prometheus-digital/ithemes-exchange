7c064d810401aa75fbde55e9548b6b9992cc5ee6
Chris Jean <gaarai@gaarai.com>
2017-01-24 12:19:21 -0600

    Preparing hotfix for use as version 1.36.2.

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: iThemes Exchange
- * Version: 1.36.1
+ * Version: 1.36.2
  * Text Domain: it-l10n-ithemes-exchange
  * Description: Easily sell your digital goods with iThemes Exchange, simple ecommerce for WordPress
  * Plugin URI: http://ithemes.com/exchange/
