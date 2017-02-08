b4515795387195760eaf9787d150dc58fd2a91c7
Timothy Jacobs <timothy@ironbounddesigns.com>
2017-02-08 13:39:59 -0500

    Mark deprecated version as 1.37.0

--- deprecated/init.php
+++ deprecated/init.php
@@ -8,7 +8,7 @@
 */
 class IT_Exchange {
 
-	var $_version         = '1.36.1';
+	var $_version         = '1.37.0';
 	var $_wp_minimum      = '3.5';
 	var $_slug            = 'ithemes-exchange';
 	var $_name            = 'iThemes Exchange';
@@ -60,6 +60,7 @@ class IT_Exchange {
 
 		do_action( 'it_exchange_loaded' );
 		add_action( 'it_libraries_loaded', array( $this, 'addons_init' ) );
+		add_action( 'it_libraries_loaded', array( $this, 'remove_dev_dirs' ) );
 	}
 
 	/**
@@ -150,6 +151,22 @@ class IT_Exchange {
 			}
 		}
 		do_action( 'it_exchange_enabled_addons_loaded' );
+	}
+
+	function remove_dev_dirs() {
+		$plugins_dir = dirname( $this->_plugin_path, 2 );
+
+		$dirs = array(
+			$this->_plugin_path . '/vendor/phpunit',
+			$plugins_dir . '/exchange-addon-membership/vendor/phpunit',
+		);
+
+		foreach ( $dirs as $dir ) {
+			if ( is_dir( $dir ) ) {
+				it_classes_load( 'it-file-utility.php' );
+				ITFileUtility::delete_directory( $dir );
+			}
+		}
 	}
 }
 
