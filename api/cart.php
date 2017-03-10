e8af4262a8e64433d228c04be6a16a67ed519145
Timothy Jacobs <timothy@ironbounddesigns.com>
2017-03-10 16:08:05 -0500

    Fix loading of the current cart.

--- api/cart.php
+++ api/cart.php
@@ -24,8 +24,19 @@ function it_exchange_get_current_cart( $create_if_not_started = true ) {
 	/** @var ITE_Cart $cart */
 	static $cart = null;
 
-	if ( $create_if_not_started && ( $cart === null || ! $cart->get_id() ) ) {
-		$cart = ITE_Cart::create();
+	$cart_id = it_exchange_get_cart_id();
+
+	if ( ( $create_if_not_started || $cart_id ) && ( $cart === null || ! $cart->get_id() ) ) {
+
+		if ( $cart_id ) {
+			$cart = new ITE_Cart(
+				new ITE_Line_Item_Session_Repository( it_exchange_get_session(), new ITE_Line_Item_Repository_Events() ),
+				$cart_id,
+				it_exchange_get_current_customer()
+			);
+		} else {
+			$cart = ITE_Cart::create();
+		}
 
 		/**
 		 * Fires when a cart is constructed.
