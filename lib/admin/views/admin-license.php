6d4c0647624a3b88ad83450b4ade8a10aafd69b4
AJ Morris <ajmorris2002@gmail.com>
2017-10-07 16:32:37 -0400

    Removing references of 2checkout.
    I’m thinking this is going to cause problems but we’ll see.

--- lib/admin/views/admin-license.php
+++ lib/admin/views/admin-license.php
@@ -37,20 +37,18 @@
       ?>
       <div class="wrap">
           <?php screen_icon( 'it-exchange' ); ?>
-
           <?php do_action( 'it_exchange_paypa-pro_settings_page_top' ); ?>
           <?php do_action( 'it_exchange_addon_settings_page_top' ); ?>
 
-          <?php $form->start_form( $form_options, 'it-exchange-2checkout-settings' ); ?>
-              <?php do_action( 'it_exchange_2checkout_settings_form_top' ); ?>
+          <?php $form->start_form( $form_options, 'it-exchange-licenses-settings' ); ?>
+              <?php do_action( 'it_exchange_licenses_settings_form_top' ); ?>
               <?php get_form_table( $form, $form_values ); ?>
-              <?php #settings_fields('exchange_2checkout_license'); ?>
-              <?php do_action( 'it_exchange_2checkout_settings_form_bottom' ); ?>
+              <?php do_action( 'it_exchange_licenses_settings_form_bottom' ); ?>
               <p class="submit">
                   <?php $form->add_submit( 'submit', array( 'value' => __( 'Save Changes', 'LION' ), 'class' => 'button button-primary button-large' ) ); ?>
               </p>
           <?php $form->end_form(); ?>
-          <?php do_action( 'it_exchange_2checkout_settings_page_bottom' ); ?>
+          <?php do_action( 'it_exchange_licenses_settings_page_bottom' ); ?>
           <?php do_action( 'it_exchange_addon_settings_page_bottom' ); ?>
       </div>
       <?php
@@ -73,23 +71,23 @@ function get_form_table( $form, $settings = array() ) {
     <!-- This is where the form would start for all of the licenses. -->
 
         <?php
-            // $exchangewp_2checkout_options = get_option( 'it-storage-exchange_addon_2checkout' );
-            // $license = $exchangewp_2checkout_options['2checkout_license'];
+            // $exchangewp_licenses_options = get_option( 'it-storage-exchange_addon_licenses' );
+            // $license = $exchangewp_licenses_options['licenses_license'];
             // var_dump($license);
-            // $exstatus = trim( get_option( 'exchange_2checkout_license_status' ) );
+            // $exstatus = trim( get_option( 'exchange_licenses_license_status' ) );
             // var_dump($exstatus);
          ?>
         <p>
-          <label class="description" for="exchange_2checkout_license_key"><?php _e('Enter your license key'); ?></label>
-          <?php $form->add_text_box( '2checkout_license' ); ?>
+          <label class="description" for="exchange_licenses_license_key"><?php _e('Enter your license key'); ?></label>
+          <?php $form->add_text_box( 'licenses_license' ); ?>
           <span>
             <?php if( $exstatus !== false && $exstatus == 'valid' ) { ?>
 							<span style="color:green;"><?php _e('active'); ?></span>
-							<?php wp_nonce_field( 'exchange_2checkout_nonce', 'exchange_2checkout_nonce' ); ?>
-							<input type="submit" class="button-secondary" name="exchange_2checkout_license_deactivate" value="<?php _e('Deactivate License'); ?>"/>
+							<?php wp_nonce_field( 'exchange_licenses_nonce', 'exchange_licenses_nonce' ); ?>
+							<input type="submit" class="button-secondary" name="exchange_licenses_license_deactivate" value="<?php _e('Deactivate License'); ?>"/>
 						<?php } else {
-							wp_nonce_field( 'exchange_2checkout_nonce', 'exchange_2checkout_nonce' ); ?>
-							<input type="submit" class="button-secondary" name="exchange_2checkout_license_activate" value="<?php _e('Activate License'); ?>"/>
+							wp_nonce_field( 'exchange_licenses_nonce', 'exchange_licenses_nonce' ); ?>
+							<input type="submit" class="button-secondary" name="exchange_licenses_license_activate" value="<?php _e('Activate License'); ?>"/>
 						<?php } ?>
           </span>
         </p>
@@ -103,17 +101,17 @@ function get_form_table( $form, $settings = array() ) {
  * @return void
 */
 function save_settings() {
-    $defaults = it_exchange_get_option( 'addon_2checkout' );
+    $defaults = it_exchange_get_option( 'addon_licenses' );
     $new_values = wp_parse_args( ITForm::get_post_data(), $defaults );
 
     // Check nonce
-    if ( !wp_verify_nonce( $_POST['_wpnonce'], 'it-exchange-2checkout-settings' ) ) {
+    if ( !wp_verify_nonce( $_POST['_wpnonce'], 'it-exchange-licenses-settings' ) ) {
         $this->error_message = __( 'Error. Please try again', 'LION' );
         return;
     }
 
-    $errors = apply_filters( 'it_exchange_add_on_2checkout_validate_settings', $this->get_form_errors( $new_values ), $new_values );
-    if ( !$errors && it_exchange_save_option( 'addon_2checkout', $new_values ) ) {
+    $errors = apply_filters( 'it_exchange_add_on_licenses_validate_settings', $this->get_form_errors( $new_values ), $new_values );
+    if ( !$errors && it_exchange_save_option( 'addon_licenses', $new_values ) ) {
         ITUtility::show_status_message( __( 'Settings saved.', 'LION' ) );
     } else if ( $errors ) {
         $errors = implode( '<br />', $errors );
@@ -123,21 +121,21 @@ function save_settings() {
     }
     // This is for all things licensing check
     // listen for our activate button to be clicked
-  	if( isset( $_POST['exchange_2checkout_license_activate'] ) ) {
+  	if( isset( $_POST['exchange_licenses_license_activate'] ) ) {
 
   		// run a quick security check
-  	 	if( ! check_admin_referer( 'exchange_2checkout_nonce', 'exchange_2checkout_nonce' ) )
+  	 	if( ! check_admin_referer( 'exchange_licenses_nonce', 'exchange_licenses_nonce' ) )
   			return; // get out if we didn't click the Activate button
 
   		// retrieve the license from the database
-      $exchangewp_2checkout_options = get_option( 'it-storage-exchange_addon_2checkout' );
-      $license = trim( $exchangewp_2checkout_options['2checkout_license'] );
+      $exchangewp_licenses_options = get_option( 'it-storage-exchange_addon_licenses' );
+      $license = trim( $exchangewp_licenses_options['licenses_license'] );
 
   		// data to send in our API request
   		$api_params = array(
   			'edd_action' => 'activate_license',
   			'license'    => $license,
-  			'item_name'  => urlencode( '2checkout' ), // the name of our product in EDD
+  			'item_name'  => urlencode( 'licenses' ), // the name of our product in EDD
   			'url'        => home_url()
   		);
 
@@ -187,7 +185,7 @@ function save_settings() {
 
   					case 'item_name_mismatch' :
 
-  						$message = sprintf( __( 'This appears to be an invalid license key for %s.' ), '2checkout' );
+  						$message = sprintf( __( 'This appears to be an invalid license key for %s.' ), 'licenses' );
   						break;
 
   					case 'no_activations_left':
@@ -207,7 +205,7 @@ function save_settings() {
 
   		// Check if anything passed on a message constituting a failure
   		if ( ! empty( $message ) ) {
-  			$base_url = admin_url( 'admin.php?page=' . '2checkout-license' );
+  			$base_url = admin_url( 'admin.php?page=' . 'licenses-license' );
   			$redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
 
   			// wp_redirect( $redirect );
@@ -216,32 +214,32 @@ function save_settings() {
   		}
 
   		//$license_data->license will be either "valid" or "invalid"
-  		update_option( 'exchange_2checkout_license_status', $license_data->license );
-  		// wp_redirect( admin_url( 'admin.php?page=' . '2checkout-license' ) );
+  		update_option( 'exchange_licenses_license_status', $license_data->license );
+  		// wp_redirect( admin_url( 'admin.php?page=' . 'licenses-license' ) );
   		// exit();
       return;
   	}
 
     // deactivate here
     // listen for our activate button to be clicked
-  	if( isset( $_POST['exchange_2checkout_license_deactivate'] ) ) {
+  	if( isset( $_POST['exchange_licenses_license_deactivate'] ) ) {
 
   		// run a quick security check
-  	 	if( ! check_admin_referer( 'exchange_2checkout_nonce', 'exchange_2checkout_nonce' ) )
+  	 	if( ! check_admin_referer( 'exchange_licenses_nonce', 'exchange_licenses_nonce' ) )
   			return; // get out if we didn't click the Activate button
 
   		// retrieve the license from the database
-  		// $license = trim( get_option( 'exchange_2checkout_license_key' ) );
+  		// $license = trim( get_option( 'exchange_licenses_license_key' ) );
 
-      $exchangewp_2checkout_options = get_option( 'it-storage-exchange_addon_2checkout' );
-      $license = $exchangewp_2checkout_options['2checkout_license'];
+      $exchangewp_licenses_options = get_option( 'it-storage-exchange_addon_licenses' );
+      $license = $exchangewp_licenses_options['licenses_license'];
 
 
   		// data to send in our API request
   		$api_params = array(
   			'edd_action' => 'deactivate_license',
   			'license'    => $license,
-  			'item_name'  => urlencode( '2checkout' ), // the name of our product in EDD
+  			'item_name'  => urlencode( 'licenses' ), // the name of our product in EDD
   			'url'        => home_url()
   		);
   		// Call the custom API.
@@ -256,10 +254,10 @@ function save_settings() {
   				$message = __( 'An error occurred, please try again.' );
   			}
 
-  			// $base_url = admin_url( 'admin.php?page=' . '2checkout-license' );
+  			// $base_url = admin_url( 'admin.php?page=' . 'licenses-license' );
   			// $redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
 
-  			// wp_redirect( 'admin.php?page=2checkout-license' );
+  			// wp_redirect( 'admin.php?page=licenses-license' );
   			// exit();
         return;
   		}
@@ -268,10 +266,10 @@ function save_settings() {
   		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
   		// $license_data->license will be either "deactivated" or "failed"
   		if( $license_data->license == 'deactivated' ) {
-  			delete_option( 'exchange_2checkout_license_status' );
+  			delete_option( 'exchange_licenses_license_status' );
   		}
 
-  		// wp_redirect( admin_url( 'admin.php?page=' . '2checkout-license' ) );
+  		// wp_redirect( admin_url( 'admin.php?page=' . 'licenses-license' ) );
   		// exit();
       return;
 
@@ -284,7 +282,7 @@ function save_settings() {
  *
  * @since 1.2.2
  */
-function exchange_2checkout_admin_notices() {
+function exchange_licenses_admin_notices() {
 	if ( isset( $_GET['sl_activation'] ) && ! empty( $_GET['message'] ) ) {
 
 		switch( $_GET['sl_activation'] ) {
