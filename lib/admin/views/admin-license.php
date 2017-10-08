74f38b7eaf7e1d6f00bc7bac2571f644819e7cdc
AJ Morris <ajmorris2002@gmail.com>
2017-10-08 08:09:26 -0400

    Trying to get the save functionality of the form to work.

--- lib/admin/views/admin-license.php
+++ lib/admin/views/admin-license.php
@@ -67,7 +67,6 @@ function get_form_table( $form, $settings = array() ) {
 		}
 
     ?>
-
     <!-- This is where the form would start for all of the licenses. -->
     <table class="form-table">
       <tbody>
@@ -78,28 +77,13 @@ function get_form_table( $form, $settings = array() ) {
         </tr>
       </tbody>
     </table>
-        <?php
-            // $exchangewp_licenses_options = get_option( 'it-storage-exchange_addon_licenses' );
-            // $license = $exchangewp_licenses_options['licenses_license'];
-            // var_dump($license);
-            // $exstatus = trim( get_option( 'exchange_licenses_license_status' ) );
-            // var_dump($exstatus);
-         ?>
-        <p>
-          <label class="description" for="exchange_licenses_license_key"><?php _e('Enter your license key'); ?></label>
-          <!-- <?#php $form->add_text_box( 'licenses_license' ); ?>
-          <span>
-            <?php if( $exstatus !== false && $exstatus == 'valid' ) { ?>
-							<span style="color:green;"><?php _e('active'); ?></span>
-							<?php wp_nonce_field( 'exchange_licenses_nonce', 'exchange_licenses_nonce' ); ?>
-							<input type="submit" class="button-secondary" name="exchange_licenses_license_deactivate" value="<?php _e('Deactivate License'); ?>"/>
-						<?php } else {
-							wp_nonce_field( 'exchange_licenses_nonce', 'exchange_licenses_nonce' ); ?>
-							<input type="submit" class="button-secondary" name="exchange_licenses_license_activate" value="<?php _e('Activate License'); ?>"/>
-						<?php } ?>
-          </span>
-        </p> -->
     <?php
+}
+
+//need to change the if line
+if ( !empty( $_POST ) && $this->_is_admin && 'it-exchange-addons' == $this->_current_page && '2checkout' == $this->_current_add_on ) {
+    add_action( 'it_exchange_save_licenses', 'save_settings' ) );
+    do_action( 'it_exchange_save_licenses' );
 }
 
 /**
@@ -109,7 +93,7 @@ function get_form_table( $form, $settings = array() ) {
  * @return void
 */
 function save_settings() {
-    $defaults = it_exchange_get_option( 'addon_licenses' );
+    $defaults = it_exchange_get_option( 'exchangewp_licenses' );
     $new_values = wp_parse_args( ITForm::get_post_data(), $defaults );
 
     // Check nonce
@@ -119,7 +103,7 @@ function save_settings() {
     }
 
     $errors = apply_filters( 'it_exchange_add_on_licenses_validate_settings', $this->get_form_errors( $new_values ), $new_values );
-    if ( !$errors && it_exchange_save_option( 'addon_licenses', $new_values ) ) {
+    if ( !$errors && it_exchange_save_option( 'exchangewp_licenses', $new_values ) ) {
         ITUtility::show_status_message( __( 'Settings saved.', 'LION' ) );
     } else if ( $errors ) {
         $errors = implode( '<br />', $errors );
@@ -127,162 +111,6 @@ function save_settings() {
     } else {
         $this->status_message = __( 'Settings not saved.', 'LION' );
     }
-    // This is for all things licensing check
-    // listen for our activate button to be clicked
-  	if( isset( $_POST['exchange_licenses_license_activate'] ) ) {
-
-  		// run a quick security check
-  	 	if( ! check_admin_referer( 'exchange_licenses_nonce', 'exchange_licenses_nonce' ) )
-  			return; // get out if we didn't click the Activate button
-
-  		// retrieve the license from the database
-      $exchangewp_licenses_options = get_option( 'it-storage-exchange_addon_licenses' );
-      $license = trim( $exchangewp_licenses_options['licenses_license'] );
-
-  		// data to send in our API request
-  		$api_params = array(
-  			'edd_action' => 'activate_license',
-  			'license'    => $license,
-  			'item_name'  => urlencode( 'licenses' ), // the name of our product in EDD
-  			'url'        => home_url()
-  		);
-
-  		// Call the custom API.
-  		$response = wp_remote_post( 'https://exchangewp.com', array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
-
-  		// make sure the response came back okay
-  		if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
-
-  			if ( is_wp_error( $response ) ) {
-  				$message = $response->get_error_message();
-  			} else {
-  				$message = __( 'An error occurred, please try again.' );
-  			}
-
-  		} else {
-
-  			$license_data = json_decode( wp_remote_retrieve_body( $response ) );
-
-  			if ( false === $license_data->success ) {
-
-  				switch( $license_data->error ) {
-
-  					case 'expired' :
-
-  						$message = sprintf(
-  							__( 'Your license key expired on %s.' ),
-  							date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
-  						);
-  						break;
-
-  					case 'revoked' :
-
-  						$message = __( 'Your license key has been disabled.' );
-  						break;
-
-  					case 'missing' :
-
-  						$message = __( 'Invalid license.' );
-  						break;
-
-  					case 'invalid' :
-  					case 'site_inactive' :
-
-  						$message = __( 'Your license is not active for this URL.' );
-  						break;
-
-  					case 'item_name_mismatch' :
-
-  						$message = sprintf( __( 'This appears to be an invalid license key for %s.' ), 'licenses' );
-  						break;
-
-  					case 'no_activations_left':
-
-  						$message = __( 'Your license key has reached its activation limit.' );
-  						break;
-
-  					default :
-
-  						$message = __( 'An error occurred, please try again.' );
-  						break;
-  				}
-
-  			}
-
-  		}
-
-  		// Check if anything passed on a message constituting a failure
-  		if ( ! empty( $message ) ) {
-  			$base_url = admin_url( 'admin.php?page=' . 'licenses-license' );
-  			$redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
-
-  			// wp_redirect( $redirect );
-  			// exit();
-        return;
-  		}
-
-  		//$license_data->license will be either "valid" or "invalid"
-  		update_option( 'exchange_licenses_license_status', $license_data->license );
-  		// wp_redirect( admin_url( 'admin.php?page=' . 'licenses-license' ) );
-  		// exit();
-      return;
-  	}
-
-    // deactivate here
-    // listen for our activate button to be clicked
-  	if( isset( $_POST['exchange_licenses_license_deactivate'] ) ) {
-
-  		// run a quick security check
-  	 	if( ! check_admin_referer( 'exchange_licenses_nonce', 'exchange_licenses_nonce' ) )
-  			return; // get out if we didn't click the Activate button
-
-  		// retrieve the license from the database
-  		// $license = trim( get_option( 'exchange_licenses_license_key' ) );
-
-      $exchangewp_licenses_options = get_option( 'it-storage-exchange_addon_licenses' );
-      $license = $exchangewp_licenses_options['licenses_license'];
-
-
-  		// data to send in our API request
-  		$api_params = array(
-  			'edd_action' => 'deactivate_license',
-  			'license'    => $license,
-  			'item_name'  => urlencode( 'licenses' ), // the name of our product in EDD
-  			'url'        => home_url()
-  		);
-  		// Call the custom API.
-  		$response = wp_remote_post( 'https://exchangewp.com', array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
-
-  		// make sure the response came back okay
-  		if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
-
-  			if ( is_wp_error( $response ) ) {
-  				$message = $response->get_error_message();
-  			} else {
-  				$message = __( 'An error occurred, please try again.' );
-  			}
-
-  			// $base_url = admin_url( 'admin.php?page=' . 'licenses-license' );
-  			// $redirect = add_query_arg( array( 'sl_activation' => 'false', 'message' => urlencode( $message ) ), $base_url );
-
-  			// wp_redirect( 'admin.php?page=licenses-license' );
-  			// exit();
-        return;
-  		}
-
-  		// decode the license data
-  		$license_data = json_decode( wp_remote_retrieve_body( $response ) );
-  		// $license_data->license will be either "deactivated" or "failed"
-  		if( $license_data->license == 'deactivated' ) {
-  			delete_option( 'exchange_licenses_license_status' );
-  		}
-
-  		// wp_redirect( admin_url( 'admin.php?page=' . 'licenses-license' ) );
-  		// exit();
-      return;
-
-  	}
-
 }
 
 /**
